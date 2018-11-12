c278e7903592d28f1c5b1796bfe12e80b0696a37
George Knee <georgeknee@googlemail.com>
2018-11-12 09:59:45 +0000

    added tests of areDisjoint and madeStrictlyOneMark

--- packages/tictactoe/test/testTTT.js
+++ packages/tictactoe/test/testTTT.js
@@ -6,7 +6,6 @@ contract('TicTacToehelpers', (accounts) => {
 
   before(async () => {
     TTT = await TTT.deployed();
-
   });
 
   it("Recognizes a winning 'marks' integer after 3 marks", async () => {
@@ -18,22 +17,28 @@ contract('TicTacToehelpers', (accounts) => {
   });
 
   it("Recognizes a winning 'marks' integer after 5 marks", async () => {
-    assert.isTrue(await TTT.hasWon.call(0b000000000));
+    assert.isTrue(await TTT.hasWon.call(0b111110000));
   });
 
-  // it("Recognizes disjoint noughts and crosses", async () => {
-  //   assert.isTrue(await TTT.areDisjoint.call(0b000000111,0b111000000));
-  // });
-//   it("Recognizes a winning 'marks' integer after 4 marks", function() {
-//     TTT.new().then(function(instance) {
-//       assert.true(instance.hasWon.call(0b100000111), "didn't recognize a winning 'marks' integer");
-//     });
-//   });
-//   it("Recognizes a winning 'marks' integer after 5 marks", function() {
-//     TTT.new(). then(function(instance) {
-//       assert.true(instance.hasWon.call(0b000000000), "didn't recognize a winning 'marks' integer");
-//     });
-//   });
+  it("Rejects a non-winning 'marks' integer after 3 marks", async () => {
+    assert.isFalse(await TTT.hasWon.call(0b110010000));
+  });
+
+  it("Recognizes disjoint noughts and crosses", async () => {
+    assert.isTrue(await TTT.areDisjoint.call(0b000000111,0b111000000));
+  });
+
+  it("Rejects overlapping noughts and crosses", async () => {
+    assert.isFalse(await TTT.areDisjoint.call(0b000000001,0b100000001));
+  });
+
+  it("Recognizes valid move", async () => {
+    assert.isTrue(await TTT.madeStrictlyOneMark.call(0b000110000,0b000111000));
+  });
+
+  it("Rejects deletion of marks", async () => {
+    assert.isFalse(await TTT.madeStrictlyOneMark.call(0b100000001,0b110000000));
+  });
 });
 
 /* IN truffle develop console: truffle deploy, followed by TicTacToehelpers.at("0xa4392264a2d8c998901d10c154c91725b1bf0158").hasWon(0b111000000) returns  true */
