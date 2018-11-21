b7b638d67e578b077735fe7a215317c6357068aa
George Knee <georgeknee@googlemail.com>
2018-11-21 15:28:33 +0000

    import core TypeScript from rps-poc, and begin modifying to reflect Tic Tac Toe logic and conventions

---
+++ packages/tictactoe/src/core/encode.ts
@@ -0,0 +1,104 @@
+// TODO change for Tic Tac Toe instead of Rock Paper Scissors
+// to do this, one needs to understand the attributes that each position should have. 
+
+import { Channel, State, toHex32, padBytes32 } from 'fmg-core';
+import * as positions from './positions';
+// import { Move } from './moves';
+import hexToBN from './hexToBN';
+
+export default function encode(position: positions.Position) {
+  const { libraryAddress, channelNonce, participants, turnNum, balances } = position;
+  const channel = new Channel(libraryAddress, channelNonce, participants);
+
+  const stateCount = ('stateCount' in position) ? position.stateCount : 0;
+
+  const state = new State({
+    channel,
+    stateType: stateType(position),
+    turnNum,
+    stateCount,
+    resolution: balances.map(hexToBN),
+  });
+
+  return state.toHex() + encodeGameAttributes(position);
+}
+
+function stateType(position: positions.Position) {
+  switch (position.name) {
+    case positions.PRE_FUND_SETUP_A:
+    case positions.PRE_FUND_SETUP_B:
+      return State.StateType.PreFundSetup;
+    case positions.POST_FUND_SETUP_A:
+    case positions.POST_FUND_SETUP_B:
+      return State.StateType.PostFundSetup;
+    case positions.VICTORY:
+      return State.StateType.Conclude;
+    case positions.DRAW:
+      return State.StateType.Conclude;
+    default:
+      return State.StateType.Game;
+  }
+}
+
+function encodeGameAttributes(position: positions.Position) {
+  switch (position.name) {
+    case positions.PROPOSE:
+      return packProposeAttributes(position);
+    case positions.ACCEPT:
+      return packAcceptAttributes(position);
+    case positions.PLAYING:
+      return packRevealAttributes(position);
+    case positions.VICTORY:
+      return '';
+    case positions.DRAW:
+      return '';
+    default:
+      // unreachable
+      return packRestingAttributes(position.roundBuyIn);
+  }
+}
+
+export enum GamePositionType {
+  Resting = 0,
+  Propose = 1,
+  Accept  = 2,
+  Playing = 3 // I didn't include either of the concluding states (Victory and Draw)
+}
+
+export function packRestingAttributes(stake: string) {
+  return toHex32(GamePositionType.Resting).substr(2) + stake.substr(2); // possible issues here in future when there are more than four GamePositionTypes? substr(2) may need to become substr(>2)
+}
+
+export function packProposeAttributes(position: positions.Propose) {
+  const { roundBuyIn } = position;
+  return (
+    toHex32(GamePositionType.Propose).substr(2) +
+    padBytes32(roundBuyIn).substr(2)
+  );``
+}
+
+export function packAcceptAttributes(position: positions.Accept) {
+  const { roundBuyIn } = position;
+  return (
+    toHex32(GamePositionType.Accept).substr(2) +
+    padBytes32(roundBuyIn).substr(2) 
+  );
+}
+
+export function packRevealAttributes(position: positions.Playing) {
+  const { roundBuyIn, noughts, crosses } = position;
+  return (
+    toHex32(GamePositionType.Playing).substr(2) +
+    padBytes32(roundBuyIn).substr(2) +
+    toHex32(noughts).substr(2) +
+    toHex32(crosses).substr(2) 
+  );
+}
+
+// export function hashCommitment(move: Move, salt: string) {
+//   return soliditySha3(
+//     { type: 'uint256', value: move },
+//     { type: 'bytes32', value: padBytes32(salt) },
+//   );
+// }
+
---
+++ packages/tictactoe/src/core/results.ts
@@ -0,0 +1,102 @@
+// import { Marks } from './marks';
+import { Player } from './players';
+import BN from 'bn.js'; // TODO troubleshoot this. Need to add @types to project? yarn add @types/node ?
+
+export enum Result {
+  Tie,
+  YouWin,
+  YouLose,
+}
+
+export enum AbsoluteResult {
+  Tie,
+  AWins,
+  BWins,
+}
+
+const topRow: number = 448; /*  0b111000000 = 448 mask for win @ row 1 */
+const midRow: number =  56; /*  0b000111000 =  56 mask for win @ row 2 */
+const botRow: number =   7; /*  0b000000111 =   7 mask for win @ row 3 */
+const lefCol: number = 292; /*  0b100100100 = 292 mask for win @ col 1 */
+const midCol: number = 146; /*  0b010010010 = 146 mask for win @ col 2 */
+const rigCol: number =  73; /*  0b001001001 =  73 mask for win @ col 3 */
+const dhDiag: number = 273; /*  0b100010001 = 273 mask for win @ downhill diag */
+const uhDiag: number =  84; /*  0b001010100 =  84 mask for win @ uphill diag */
+//
+const fullBd: number = 511; /* 0b111111111 = 511 full board */
+
+export function isWinningMarks(marks: number){
+  return  (
+    ((marks & topRow) == topRow) ||
+    ((marks & midRow) == midRow) ||
+    ((marks & botRow) == botRow) ||
+    ((marks & lefCol) == lefCol) ||
+    ((marks & midCol) == midCol) ||
+    ((marks & rigCol) == rigCol) ||
+    ((marks & dhDiag) == dhDiag) ||
+    ((marks & uhDiag) == uhDiag) 
+  )
+}
+
+export function calculateResult(yourMarks: number, theirMarks: number, yourVictoryClaim: boolean, theirVictoryClaim: boolean): Result {
+  if (yourVictoryClaim && !theirVictoryClaim && isWinningMarks(yourMarks)) {
+    return Result.YouWin;
+  }
+  else if (!yourVictoryClaim && theirVictoryClaim && isWinningMarks(theirMarks)) { 
+    return Result.YouLose;
+  }
+  else {
+    return Result.Tie;
+  }
+}
+
+export function calculateAbsoluteResult(asMarks: number, bsMarks: number, asVictoryClaim: boolean, bsVictoryClaim: boolean): AbsoluteResult {
+  if (asVictoryClaim && !bsVictoryClaim && isWinningMarks(asMarks)) {
+    return AbsoluteResult.AWins;
+  }
+  else if (!asVictoryClaim && bsVictoryClaim && isWinningMarks(bsMarks)) { 
+    return AbsoluteResult.BWins;
+  }
+  else {
+    return AbsoluteResult.Tie;
+  }
+}
+
+
+export function convertToAbsoluteResult(relativeResult: Result, youAre: Player) {
+  const youArePlayerA = youAre === Player.PlayerA;
+
+  switch(relativeResult) {
+    case Result.Tie:
+      return AbsoluteResult.Tie;
+    case Result.YouWin:
+      return youArePlayerA ? AbsoluteResult.AWins : AbsoluteResult.BWins;
+    case Result.YouLose:
+      return youArePlayerA ? AbsoluteResult.BWins : AbsoluteResult.AWins;
+  }
+}
+
+export function convertToRelativeResult(absoluteResult: AbsoluteResult, youAre: Player): Result {
+  const youArePlayerA = youAre === Player.PlayerA;
+
+  switch(absoluteResult) {
+    case AbsoluteResult.Tie:
+      return Result.Tie;
+    case AbsoluteResult.AWins:
+      return youArePlayerA ? Result.YouWin : Result.YouLose;
+    case AbsoluteResult.BWins:
+      return youArePlayerA ? Result.YouLose : Result.YouWin;
+  }
+
+}
+
+export function balancesAfterResult(absoluteResult: AbsoluteResult, roundBuyIn: BN, balances: [BN, BN]): [BN, BN] {
+  switch(absoluteResult) {
+    case AbsoluteResult.AWins:
+      return [ balances[0].add(roundBuyIn.muln(2)), balances[1].sub(roundBuyIn.muln(2)) ];
+    case AbsoluteResult.BWins:
+      return balances;
+    case AbsoluteResult.Tie:
+      return [ balances[0].add(roundBuyIn.muln(1)), balances[1].sub(roundBuyIn.muln(1)) ];
+  }
+}
