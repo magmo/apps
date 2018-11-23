62048c6e4f12673a1b1d09343c2551f1c327e4f9
George Knee <georgeknee@googlemail.com>
2018-11-23 18:14:20 +0000

    got truffle tests to run by updating dev environment including babel

--- packages/tictactoe/truffle.js
+++ packages/tictactoe/truffle.js
@@ -1,17 +1,27 @@
+// We only want this loaded in a test environment
+// so we can load and parse this file from the app
+require('dotenv').config()
+
+if (process.env.NODE_ENV === 'test') {
+    require('ts-node/register'); 
+  }
+
+require("babel-register"); // To handle es6 syntax in the tests
+
 module.exports = {
   networks: {
-      development: {
-          host: "localhost",
-          port: 7545, // Using ganache as development network
-          network_id: "*",
-          gas: 80000000,
-          gasPrice: 25000000000
-      }
+    development: {
+      host: "localhost",
+      port: 7545, // Using ganache as development network
+      network_id: "*",
+      gas: 80000000,
+      gasPrice: 25000000000
+    }
   },
   solc: {
-      optimizer: {
-          enabled: true,
-          runs: 200
-      }
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
   }
 };
