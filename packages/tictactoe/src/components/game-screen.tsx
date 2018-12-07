d417700766ad25d944ef0359eb0eb6f4be2d61db
George Knee <georgeknee@googlemail.com>
2018-12-07 16:11:52 +0000

    added magmo logo

--- packages/tictactoe/src/components/game-screen.tsx
+++ packages/tictactoe/src/components/game-screen.tsx
@@ -3,6 +3,7 @@ import Board from './board';
 import Outcome from './outcome';
 import StatusAndBalances from './statusAndBalances';
 import { Marks, Marker, Result, Player } from '../core';
+import MAGMO_LOGO from '../images/magmo_logo.svg';
 
 interface Props {
   stateType: string;
@@ -21,6 +22,7 @@ export default class GameScreen extends React.PureComponent<Props> {
     <div>
     <StatusAndBalances stateType="blah" balances={balances} player={player} you = {you}/>
     <Board stateType="blah" noughts={noughts} crosses={crosses}/>
+    <div id="magmo-logo"><img src={MAGMO_LOGO}/></div>
     <Outcome stateType="blah" result={result} />
     </div>
     );
