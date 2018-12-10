d3c7865a615bb14a19d90bb8ad4692f71cb23cbb
George Knee <georgeknee@googlemail.com>
2018-12-10 21:51:08 +0000

    use constants in board component

--- packages/tictactoe/src/components/board.tsx
+++ packages/tictactoe/src/components/board.tsx
@@ -76,33 +76,33 @@ export default class Board extends React.PureComponent<Props> {
                 <td id="tl" onClick={() => this.props.xsMoveChosen(Marks.tl)}> 
                   {this.renderMark(noughts, crosses, Marks.tl)}
                 </td>
-                <td id="tm">
-                  {this.renderMark(noughts, crosses, 0b010000000)}
+                <td id="tm" onClick={() => this.props.xsMoveChosen(Marks.tm)}>
+                  {this.renderMark(noughts, crosses, Marks.tm)}
                 </td>
-                <td id="tr">
-                  {this.renderMark(noughts, crosses, 0b001000000)}
-                </td>
-            </tr>
-            <tr>
-                <td id="ml"> 
-                  {this.renderMark(noughts, crosses, 0b000100000)}
-                </td>
-                <td id="mm">
-                  {this.renderMark(noughts, crosses, 0b000010000)}
-                </td>
-                <td id="mr">
-                  {this.renderMark(noughts, crosses, 0b000001000)}
+                <td id="tr" onClick={() => this.props.xsMoveChosen(Marks.tr)}>
+                  {this.renderMark(noughts, crosses, Marks.tr)}
                 </td>
             </tr>
             <tr>
-                <td id="bl"> 
-                  {this.renderMark(noughts, crosses, 0b000000100)}
+                <td id="ml" onClick={() => this.props.xsMoveChosen(Marks.ml)}> 
+                  {this.renderMark(noughts, crosses, Marks.ml)}
                 </td>
-                <td id="bm">
-                  {this.renderMark(noughts, crosses, 0b000000010)}
+                <td id="mm" onClick={() => this.props.xsMoveChosen(Marks.mm)}>
+                  {this.renderMark(noughts, crosses, Marks.mm)}
                 </td>
-                <td id="br">
-                  {this.renderMark(noughts, crosses, 0b000000001)}
+                <td id="mr" onClick={() => this.props.xsMoveChosen(Marks.mr)}>
+                  {this.renderMark(noughts, crosses, Marks.mr)}
+                </td>
+            </tr>
+            <tr>
+                <td id="bl" onClick={() => this.props.xsMoveChosen(Marks.bl)}> 
+                  {this.renderMark(noughts, crosses, Marks.bl)}
+                </td>
+                <td id="bm" onClick={() => this.props.xsMoveChosen(Marks.bm)}>
+                  {this.renderMark(noughts, crosses, Marks.bm)}
+                </td>
+                <td id="br" onClick={() => this.props.xsMoveChosen(Marks.br)}>
+                  {this.renderMark(noughts, crosses, Marks.br)}
                 </td>
             </tr>
         </table>
