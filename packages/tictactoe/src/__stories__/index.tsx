82eeba603fc11c3ac0bdbfd30c4bef2235aef782
George Knee <georgeknee@googlemail.com>
2019-01-14 14:13:26 +0000

    trying to bring storybook into line with wallet approach, getting error message on screens connected to the redux store

--- packages/tictactoe/src/__stories__/index.tsx
+++ packages/tictactoe/src/__stories__/index.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import SiteContainer from '../containers/SiteContainer';
 import { Provider } from 'react-redux';
-// import { Button } from '@storybook/react/demo';
-import HomePage from '../components/HomePage';
-import { OpenGame } from "../redux/open-games/state";
 import { OpenGameEntry } from '../components/OpenGameCard';
-import Button from '../components/Button';
-import Board from '../components/Board';
-import { YourMarker, TheirMarker } from '../components/Marker';
-import Outcome from '../components/Outcome';
-import StatusAndBalances from '../components/StatusAndBalances';
-import GameScreen from '../components/GameScreen';
-import InsufficientFunds from '../components/InsufficientFunds';
-import { Marker, Result, Player, Imperative, Marks } from '../core';
-import { scenarios } from '../core';
-import * as loginActions from '../redux/login/actions';
-import PlayAgain from '../components/PlayAgain';
+import *  as states from '../redux/game/state';
+import { Marker, Player, Imperative } from '../core';
 import BN from "bn.js";
 import bnToHex from "../utils/bnToHex";
-import * as states from '../redux/game/state';
-
-import GameContainer from '../containers/GameContainer';
-
+import { OpenGame } from "../redux/open-games/state";
 import '../index.scss';
 import '../index.css';
+import { scenarios } from '../core';
 
-const finneyFourSix = [new BN(4000000000000000), new BN(6000000000000000)].map(bnToHex) as [string, string]; // in wei
-const finneyFiveFive = [new BN(5000000000000000), new BN(5000000000000000)].map(bnToHex) as [string, string];
 const finneySixFour = [new BN(6000000000000000), new BN(4000000000000000)].map(bnToHex) as [string, string];
+
+const fakeStore = (state) => ({
+  dispatch: action => {
+    alert(`Action ${action.type} triggered`);
+    return action;
+  },
+  getState: () => (state),
+  subscribe: () => (() => {/* empty */ }),
+  replaceReducer: () => { /* empty */ },
+});
+
+const testState = (state) => (
+  () => (
+    <Provider store={fakeStore(state)}>
+      <SiteContainer />
+    </Provider>
+  )
+);
 
 const shared = { ...scenarios.shared };
 
@@ -39,8 +42,8 @@ const initialState = {
       stateCount: 1,
       noughts: 0,
       crosses: 0,
-      balances: finneyFiveFive,
-      onScreenBalances: finneyFiveFive,
+      balances: finneySixFour,
+      onScreenBalances: finneySixFour,
       turnNum: 4,
       result: Imperative.Choose,
       player: Player.PlayerA,
@@ -50,13 +53,7 @@ const initialState = {
 };
 
 
-const marksMade = (x: Marks) => alert("marks made");
 const joinOpenGame = () => alert("join open game");
-const playAgain = () => alert('playing again');
-const resign = () => alert('resigned');
-// const newOpenGame = () => alert("new open game");
-
-storiesOf('HomePage', module).add('Home', () => <HomePage login={loginActions.loginRequest}/>);
 
 const openGame: OpenGame = {
   address:"test address",
@@ -65,141 +62,33 @@ const openGame: OpenGame = {
   isPublic: true,
   createdAt: 0,
 };
-storiesOf('LobbyPage', module)
-  .add('Open Game Entry', () => <OpenGameEntry 
-  openGame={openGame} 
-  joinOpenGame={joinOpenGame}
-  />)
-  .add('Button', () => <Button
-  children={"Click Me"}
-  onClick={() => alert("button clicked")}
-  />);
-
-storiesOf('Board', module)
-  .add('Empty', () => <Board noughts={0} crosses={0} marksMade={marksMade} you={Marker.noughts}/>)
-  .add('Draw', () => <Board noughts={0b010011100} crosses={0b101100011} marksMade={marksMade} you={Marker.noughts} />)
-  .add('O win', () => <Board noughts={0b111000000} crosses={0b000011000} marksMade={marksMade} you={Marker.noughts} />)
-  .add('X win', () => <Board noughts={0b100010000} crosses={0b001001001} marksMade={marksMade} you={Marker.noughts} />);
-
-storiesOf('Status', module)
-  .add('Your Marker', () => <YourMarker you={Marker.crosses} />)
-  .add('Their Marker', () => <TheirMarker you={Marker.crosses} />);
-
-storiesOf('Outcome', module)
-  .add('You Win', () => <Outcome result={Result.YouWin} />)
-  .add('You Lose', () => <Outcome result={Result.YouLose} />)
-  .add('Tie', () => <Outcome result={Result.Tie} />)
-  .add('Wait', () => <Outcome result={Imperative.Wait} />)
-  .add('Choose', () => <Outcome result={Imperative.Choose} />);
-
-storiesOf('Status and Balances', module)
-  .add('You Winning', () => <StatusAndBalances onScreenBalances={finneySixFour} player={Player.PlayerA} you={Marker.crosses} />)
-  .add('You Losing', () => <StatusAndBalances onScreenBalances={finneyFourSix} player={Player.PlayerB} you={Marker.crosses} />);
 
 
-storiesOf('Game Screen', module)
-  .add('Waiting', () => <GameScreen
-    noughts={0b000100000}
-    crosses={0b000001001}
-    you={Marker.crosses}
-    player={Player.PlayerA}
-    result={Imperative.Wait}
-    onScreenBalances={finneySixFour}
-    marksMade={marksMade}
-    resign={resign}
-  />)
-  .add('Choosing', () => <GameScreen
-    noughts={0b000100010}
-    crosses={0b000001001}
-    you={Marker.crosses}
-    player={Player.PlayerA}
-    result={Imperative.Choose}
-    onScreenBalances={finneyFourSix}
-    marksMade={marksMade}
-    resign={resign}
-  />)
-  .add('X win', () => <GameScreen
-    noughts={0b100100000}
-    crosses={0b001001001}
-    you={Marker.crosses}
-    player={Player.PlayerA}
-    result={Result.YouWin}
-    onScreenBalances={finneySixFour}
-    marksMade={marksMade}
-    resign={resign}
-  />)
-  .add('Double X win', () => <GameScreen
-    noughts={0b010100110}
-    crosses={0b101011001}
-    you={Marker.crosses}
-    player={Player.PlayerA}
-    result={Result.YouWin}
-    onScreenBalances={finneySixFour}
-    marksMade={marksMade}
-    resign={resign}
-/>)
-  .add('O win', () => <GameScreen
-    noughts={0b100100100}
-    crosses={0b000011001}
-    you={Marker.crosses}
-    player={Player.PlayerA}
-    result={Result.YouLose}
-    onScreenBalances={finneyFourSix}
-    marksMade={marksMade}
-    resign={resign}
-  />)
-  .add('Tie', () => <GameScreen
-    noughts={0b101100010}
-    crosses={0b010011101}
-    you={Marker.crosses}
-    player={Player.PlayerA}
-    result={Result.Tie}
-    onScreenBalances={finneyFiveFive}
-    marksMade={marksMade}
-    resign={resign}
-  />);
+storiesOf('Lobby',module)
+.add('Open Game Entry', () => <OpenGameEntry 
+openGame={openGame} 
+joinOpenGame={joinOpenGame}
+/>);
 
-const fakeStore = (state) => ({
-  dispatch: action => {
-    alert(`Action ${action.type} triggered`);
-    return action;
-  },
-  getState: () => (state),
-  subscribe: () => (() => {/* empty */ }),
-  replaceReducer: () => { /* empty */ },
-  playAgain: () => { /* empty */ },
-  resign: () => { /* empty */ },
-});
+storiesOf('Game Screens / Crosses', module)
+  .add('Waiting', testState(states.xsWaitForOpponentToPickMove({
+    ...shared,
+    ...initialState,
+    noughts: 0b000100000,
+    crosses: 0b000001001,
+    you: Marker.crosses,
+    player: Player.PlayerA,
+    result: Imperative.Wait,
+    onScreenBalances: finneySixFour,
+    turnNum: 5,
+    balances: finneySixFour,
+    stateCount: 1,
+    twitterHandle: 'twtr',
+    roundBuyIn: '1',
+    myName: 'George',
+    opponentName: 'Mike',
+  }
+  ))
+  );
 
-const testState = (state) => (
-  () => (
-    <Provider store={fakeStore(state)}>
-      <GameContainer />
-    </Provider>
-  )
-);
 
-storiesOf('App (reading from the fake store)', module)
-  .add('test initial state', testState(initialState));
-
-  storiesOf('Post game', module)
-    .add('Play Again', () => <PlayAgain
-      noughts={0}
-      crosses={0}
-      you={Marker.noughts} // TODO this should take either value
-      player={Player.PlayerA}
-      result={Result.YouWin}
-      onScreenBalances={finneySixFour}
-      marksMade={marksMade}
-      playAgain={playAgain}
-      resign={resign}
-      />)
-    .add('Insufficient Funds', () => <InsufficientFunds
-      noughts={0}
-      crosses={0}
-      you={Marker.noughts} // TODO this should take either value
-      player={Player.PlayerA}
-      result={Result.YouWin}
-      onScreenBalances={finneySixFour}
-      marksMade={marksMade}
-      />);
