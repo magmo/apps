1763f49b50cdd0b7faec748a583d0110b61f4577
Alex Gap <alex.gap@gmail.com>
2019-01-17 12:11:43 -0800

    Remove Rules deploy from RPS

--- packages/rps-poc/migrations/2_deploy_contracts.js
+++ packages/rps-poc/migrations/2_deploy_contracts.js
@@ -1,16 +1,13 @@
 var RockPaperScissorsGame = artifacts.require("./RockPaperScissorsGame.sol");
 var RockPaperScissorsState = artifacts.require("./RockPaperScissorsState.sol");
 var State = artifacts.require("fmg-core/contracts/State.sol");
-var Rules = artifacts.require("fmg-core/contracts/Rules.sol");
+
 module.exports = function (deployer) {
   deployer.deploy(State);
-
   deployer.link(State, RockPaperScissorsState);
   deployer.deploy(RockPaperScissorsState);
   deployer.link(RockPaperScissorsState, RockPaperScissorsGame);
   deployer.link(State, RockPaperScissorsGame);
   deployer.deploy(RockPaperScissorsGame);
 
-  deployer.link(State, Rules);
-  deployer.deploy(Rules);
 };
