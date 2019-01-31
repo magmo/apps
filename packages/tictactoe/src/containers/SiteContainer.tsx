4abcae885e177dfa84603dd1efcea688cfe641ee
George Knee <georgeknee@googlemail.com>
2019-01-31 08:56:01 +0000

    do not try to set iFrame properties if it does not exist

--- packages/tictactoe/src/containers/SiteContainer.tsx
+++ packages/tictactoe/src/containers/SiteContainer.tsx
@@ -40,13 +40,13 @@ class Site extends React.PureComponent<SiteProps>{
     } else if (this.props.isAuthenticated) {
       component = <ApplicationContainer />;
       const iFrame = document.getElementById(WALLET_IFRAME_ID) as HTMLIFrameElement;
-      if (this.props.walletVisible) {
+      if (iFrame && this.props.walletVisible) {
         iFrame.style.display = 'initial';
         document.body.style.overflow = 'hidden';
         iFrame.width = '100%';
         iFrame.height = '100%';
       }
-      else {
+      else if (iFrame && !this.props.walletVisible) {
         iFrame.style.display = 'none';
         document.body.style.overflow = 'initial';
         iFrame.width = '0';
