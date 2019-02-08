81acd9ed9898d9c04d0a5c4dda95c765f3d44235
Alex Gap <alex.gap@gmail.com>
2019-02-08 09:40:06 -0800

    Update styling to follow metamask error page

--- packages/rps/src/components/LoginErrorPage.tsx
+++ packages/rps/src/components/LoginErrorPage.tsx
@@ -7,10 +7,12 @@ interface LoginErrorProps {
 
 export default function LoginErrorPage(props: LoginErrorProps) {
   return (
-    <div className="w-100 text-center mb-5">
-      <h1 className="w-100">Login Error</h1>
-      <div>
-        <p>{props.error}</p>
+    <div className="container centered-container w-100 mb-5">
+      <div className="w-100 text-center mb-5">
+        <h1 className="text-center waiting-room-title">Login Error</h1>
+        <div>
+          <p>{props.error}</p>
+        </div>
       </div>
     </div>
   );
