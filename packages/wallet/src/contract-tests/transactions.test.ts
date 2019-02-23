e0d15418e7c9ca97f483b9226f710f75fffc6a32
Alex Gap <alex.gap@gmail.com>
2019-02-22 16:07:14 -0800

    Create conclude and withdraw transaction

--- packages/wallet/src/contract-tests/transactions.test.ts
+++ packages/wallet/src/contract-tests/transactions.test.ts
@@ -5,14 +5,15 @@ import { put } from "redux-saga/effects";
 import { transactionConfirmed, transactionFinalized, transactionSentToMetamask, transactionSubmitted } from '../redux/actions';
 import { transactionSender } from "../redux/sagas/transaction-sender";
 import { signCommitment, signVerificationData } from '../utils/signing-utils';
-import { getLibraryAddress, createChallenge, concludeGame } from './test-utils';
+import { getLibraryAddress, createChallenge } from './test-utils';
 import {
-  createForceMoveTransaction, createDepositTransaction, createRespondWithMoveTransaction, createRefuteTransaction, createConcludeTransaction, createWithdrawTransaction
+  createForceMoveTransaction, createDepositTransaction, createRespondWithMoveTransaction, createRefuteTransaction, createConcludeTransaction, createWithdrawTransaction, ConcludeAndWithdrawArgs, createConcludeAndWithdrawTransaction
 } from '../utils/transaction-generator';
 
 import { depositContract } from './test-utils';
 import { Channel, Commitment, CommitmentType } from 'fmg-core';
 import { getAdjudicatorContractAddress } from '../utils/contract-utils';
+import { channelID } from 'fmg-core/lib/channel';
 
 jest.setTimeout(90000);
 
@@ -148,8 +149,8 @@ describe('transactions', () => {
     await depositContract(provider, contractAddress, participantB.address);
     const fromCommitment: Commitment = {
       channel,
-      allocation: [],
-      destination: [],
+      allocation: ['0x05', '0x05'],
+      destination: [participantA.address, participantB.address],
       turnNum: 5,
       commitmentType: CommitmentType.Conclude,
       appAttributes: '0x0',
@@ -158,8 +159,8 @@ describe('transactions', () => {
 
     const toCommitment: Commitment = {
       channel,
-      allocation: [],
-      destination: [],
+      allocation: ['0x05', '0x05'],
+      destination: [participantA.address, participantB.address],
       turnNum: 6,
       commitmentType: CommitmentType.Conclude,
       appAttributes: '0x0',
@@ -173,15 +174,54 @@ describe('transactions', () => {
   });
 
   it("should send a withdraw transaction", async () => {
-    const channel: Channel = { channelType: libraryAddress, channelNonce: getNextNonce(), participants };
-    const { channelNonce } = channel;
     const contractAddress = await getAdjudicatorContractAddress(provider);
     await depositContract(provider, contractAddress, participantA.address);
-    await depositContract(provider, contractAddress, participantB.address);
-    await concludeGame(provider, contractAddress, channelNonce, participantA, participantB);
     const senderAddress = await provider.getSigner().getAddress();
     const verificationSignature = signVerificationData(participantA.address, participantA.address, '0x01', senderAddress, participantA.privateKey);
     const withdrawTransaction = createWithdrawTransaction(contractAddress, '0x01', participantA.address, participantA.address, verificationSignature);
     await testTransactionSender(withdrawTransaction);
+  });
+
+  it("should send a conclude and withdraw transaction", async () => {
+    const channel: Channel = { channelType: libraryAddress, channelNonce: getNextNonce(), participants };
+    const channelId = channelID(channel);
+    const contractAddress = await getAdjudicatorContractAddress(provider);
+    await depositContract(provider, contractAddress, channelId);
+    const senderAddress = await provider.getSigner().getAddress();
+    const verificationSignature = signVerificationData(participantA.address, participantA.address, '0x05', senderAddress, participantA.privateKey);
+    const fromCommitment: Commitment = {
+      channel,
+      allocation: ['0x05', '0x05'],
+      destination: [participantA.address, participantB.address],
+      turnNum: 5,
+      commitmentType: CommitmentType.Conclude,
+      appAttributes: '0x0',
+      commitmentCount: 0,
+    };
+
+    const toCommitment: Commitment = {
+      channel,
+      allocation: ['0x05', '0x05'],
+      destination: [participantA.address, participantB.address],
+      turnNum: 6,
+      commitmentType: CommitmentType.Conclude,
+      appAttributes: '0x0',
+      commitmentCount: 1,
+    };
+    const fromSignature = signCommitment(fromCommitment, participantA.privateKey);
+    const toSignature = signCommitment(toCommitment, participantB.privateKey);
+
+    const args: ConcludeAndWithdrawArgs = {
+      fromCommitment,
+      toCommitment,
+      fromSignature,
+      toSignature,
+      verificationSignature,
+      participant: participantA.address,
+      destination: participantA.address,
+      amount: '0x05',
+    };
+    const concludeAndWithdrawTransaction = createConcludeAndWithdrawTransaction(contractAddress, args);
+    await testTransactionSender(concludeAndWithdrawTransaction);
   });
 });
