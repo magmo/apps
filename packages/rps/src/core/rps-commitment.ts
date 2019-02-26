486676d177b26d0d1ce4ea3c51eac202901c33f0
Alex Gap <alex.gap@gmail.com>
2019-02-26 13:00:40 -0800

    Set commitmentName  when converting from coreCommitment

--- packages/rps/src/core/rps-commitment.ts
+++ packages/rps/src/core/rps-commitment.ts
@@ -33,14 +33,11 @@ export interface RPSBaseCommitment extends BaseCommitment {
 
 export interface RPSCommitment extends RPSBaseCommitment {
   commitmentType: CommitmentType;
-  commitmentName: string;
+  commitmentName: CommitmentName;
 }
 
-
 function encodeAppAttributes(appAttrs: AppAttributes): string {
-  console.log(appAttrs);
   const { positionType, stake, preCommit, bWeapon, aWeapon, salt, } = appAttrs;
-  console.log([positionType, stake, preCommit, bWeapon, aWeapon, salt,]);
   return abi.encodeParameter(SolidityRPSCommitmentType,
     [positionType, stake, preCommit, bWeapon, aWeapon, salt,]);
 }
@@ -73,11 +70,10 @@ export function fromCoreCommitment(commitment: Commitment): RPSCommitment {
     allocation,
     destination,
     commitmentCount,
-    commitmentName: "", // TODO: Determine this based on commitment attribs
+    commitmentName: getCommitmentName(commitment),
     ...decodeAppAttributes(commitment.appAttributes),
   };
 }
-
 
 export function asCoreCommitment(rpsCommitment: RPSCommitment): Commitment {
   const {
@@ -104,4 +100,44 @@ export function asCoreCommitment(rpsCommitment: RPSCommitment): Commitment {
     commitmentCount,
     appAttributes: encodeAppAttributes({ positionType, stake, preCommit, bWeapon, aWeapon, salt }),
   };
+}
+
+
+// Commitment names
+export const PRE_FUND_SETUP_A = 'PRE_FUND_SETUP_A';
+export const PRE_FUND_SETUP_B = 'PRE_FUND_SETUP_B';
+export const POST_FUND_SETUP_A = 'POST_FUND_SETUP_A';
+export const POST_FUND_SETUP_B = 'POST_FUND_SETUP_B';
+export const APP_PROPOSE = 'APP:PROPOSE';
+export const APP_ACCEPT = 'APP:ACCEPT';
+export const APP_REVEAL = 'APP:REVEAL';
+export const APP_RESTING = 'APP:RESTING';
+export const CONCLUDE = 'CONCLUDE';
+
+export type CommitmentName = typeof PRE_FUND_SETUP_A | typeof PRE_FUND_SETUP_B | typeof POST_FUND_SETUP_A | typeof POST_FUND_SETUP_B | typeof APP_PROPOSE | typeof APP_ACCEPT | typeof APP_REVEAL | typeof APP_RESTING | typeof CONCLUDE;
+
+function getCommitmentName(commitment: Commitment): CommitmentName {
+  switch (commitment.commitmentType) {
+    case CommitmentType.PreFundSetup:
+      return commitment.commitmentCount === 0 ? PRE_FUND_SETUP_A : PRE_FUND_SETUP_B;
+    case CommitmentType.PostFundSetup:
+      return commitment.commitmentCount === 0 ? POST_FUND_SETUP_A : POST_FUND_SETUP_B;
+    case CommitmentType.Conclude:
+      return CONCLUDE;
+    case CommitmentType.App:
+      const appAttributes = decodeAppAttributes(commitment.appAttributes);
+      switch (appAttributes.positionType) {
+        case PositionType.Accepted:
+          return APP_ACCEPT;
+        case PositionType.Proposed:
+          return APP_PROPOSE;
+        case PositionType.Resting:
+          return APP_RESTING;
+        case PositionType.Reveal:
+          return APP_REVEAL;
+      }
+      break;
+
+  }
+  return CONCLUDE;
 }
