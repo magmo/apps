f2bedc1aebf3712cc498120f13b093fc8090f601
Alex Gap <alex.gap@gmail.com>
2019-03-04 16:59:43 -0800

    We can now assume ganache port is defined

--- packages/wallet/src/redux/sagas/ganache-miner.ts
+++ packages/wallet/src/redux/sagas/ganache-miner.ts
@@ -3,11 +3,8 @@ import { delay } from 'redux-saga';
 import { ethers } from 'ethers';
 
 export function* ganacheMiner() {
-  const port = process.env.DEV_GANACHE_PORT || 8545;
-  const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(`http://localhost:${port}`);
+  const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(`http://localhost:${process.env.DEV_GANACHE_PORT}`);
   const DELAY_TIME = 30000; // 30 seconds
-
-
   yield provider.send('evm_mine', {});
   yield delay(DELAY_TIME);
 }
