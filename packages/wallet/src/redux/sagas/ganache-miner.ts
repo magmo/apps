42b25507db039c9f5be628e819f141174ce4481f
Alex Gap <alex.gap@gmail.com>
2019-03-04 17:39:13 -0800

    loop miner

--- packages/wallet/src/redux/sagas/ganache-miner.ts
+++ packages/wallet/src/redux/sagas/ganache-miner.ts
@@ -5,7 +5,9 @@ import { ethers } from 'ethers';
 export function* ganacheMiner() {
   const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(`http://localhost:${process.env.DEV_GANACHE_PORT}`);
   const DELAY_TIME = 30000; // 30 seconds
-  const timeStamp = Math.round(Date.now() / 1000);
-  yield provider.send('evm_mine', timeStamp);
-  yield delay(DELAY_TIME);
+  while (true) {
+    const timeStamp = Math.round(Date.now() / 1000);
+    yield provider.send('evm_mine', timeStamp);
+    yield delay(DELAY_TIME);
+  }
 }
