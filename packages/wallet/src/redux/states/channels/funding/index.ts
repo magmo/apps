69ceef38cdad5f472c2bd91b8d705a5520b2e731
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-13 09:36:39 -0600

    states/channels/funding/index.ts is now the index for funding states

--- packages/wallet/src/redux/states/channels/funding/index.ts
+++ packages/wallet/src/redux/states/channels/funding/index.ts
@@ -1,171 +1,26 @@
-/**
- * The term 'funding' is a bit overloaded here, as it is referring to the stage of the channel, as well as
- * the actual funding of the channel.
- */
+import { SharedFundingState } from './shared';
+import { DirectFundingState, directFundingState } from './directFunding';
 
-import { MaybeFunded, channelOpen } from '../shared';
-import { DirectFundingState } from './directFunding';
-
-// stage
-export const FUNDING = 'FUNDING';
-
-// STATE TYPES
-// Funding setup
 export const WAIT_FOR_FUNDING_REQUEST = 'WAIT_FOR_FUNDING_REQUEST';
-export const APPROVE_FUNDING = 'APPROVE_FUNDING';
+export const UNKNOWN_FUNDING_TYPE = 'FUNDING_TYPE.UNKNOWN';
 
-// Funding ongoing
-export const WAIT_FOR_FUNDING_AND_POST_FUND_SETUP = 'WAIT_FOR_FUNDING_AND_POST_FUND_SETUP';
-export const WAIT_FOR_FUNDING_CONFIRMATION = 'WAIT_FOR_FUNDING_CONFIRMATION';
-export const A_WAIT_FOR_POST_FUND_SETUP = 'A_WAIT_FOR_POST_FUND_SETUP';
-export const B_WAIT_FOR_POST_FUND_SETUP = 'B_WAIT_FOR_POST_FUND_SETUP';
-
-// Possible end states
-export const ACKNOWLEDGE_FUNDING_SUCCESS = 'ACKNOWLEDGE_FUNDING_SUCCESS';
-export const SEND_FUNDING_DECLINED_MESSAGE = 'SEND_FUNDING_DECLINED_MESSAGE';
-export const ACKNOWLEDGE_FUNDING_DECLINED = 'ACKNOWLEDGE_FUNDING_DECLINED';
-
-interface BaseFundingChannelState extends MaybeFunded {
-  fundingState: DirectFundingState;
-  funded: boolean; // Though this is computable from fundingState, it might be convenient to record this on the top-level of the channel state
-}
-
-function fundingChannelState<T extends BaseFundingChannelState>(
-  params: T,
-): BaseFundingChannelState {
-  const { fundingState, funded } = params;
-  return { ...channelOpen(params), fundingState, funded };
-}
-
-export interface WaitForFundingRequest extends BaseFundingChannelState {
+export interface WaitForFundingRequest {
   type: typeof WAIT_FOR_FUNDING_REQUEST;
-  stage: typeof FUNDING;
+  fundingType: typeof UNKNOWN_FUNDING_TYPE;
+  requestedTotalFunds: string;
+  requestedYourContribution: string;
 }
 
-export interface ApproveFunding extends BaseFundingChannelState {
-  type: typeof APPROVE_FUNDING;
-  stage: typeof FUNDING;
-}
-
-export interface WaitForFundingAndPostFundSetup extends BaseFundingChannelState {
-  type: typeof WAIT_FOR_FUNDING_AND_POST_FUND_SETUP;
-  stage: typeof FUNDING;
-}
-
-export interface WaitForFundingConfirmation extends BaseFundingChannelState {
-  type: typeof WAIT_FOR_FUNDING_CONFIRMATION;
-  stage: typeof FUNDING;
-}
-
-export interface AWaitForPostFundSetup extends BaseFundingChannelState {
-  type: typeof A_WAIT_FOR_POST_FUND_SETUP;
-  stage: typeof FUNDING;
-}
-
-export interface BWaitForPostFundSetup extends BaseFundingChannelState {
-  type: typeof B_WAIT_FOR_POST_FUND_SETUP;
-  stage: typeof FUNDING;
-}
-
-export interface SendFundingDeclinedMessage extends BaseFundingChannelState {
-  type: typeof SEND_FUNDING_DECLINED_MESSAGE;
-  stage: typeof FUNDING;
-}
-
-export interface AcknowledgeFundingSuccess extends BaseFundingChannelState {
-  type: typeof ACKNOWLEDGE_FUNDING_SUCCESS;
-  stage: typeof FUNDING;
-}
-
-export interface AcknowledgeFundingDeclined extends BaseFundingChannelState {
-  type: typeof ACKNOWLEDGE_FUNDING_DECLINED;
-  stage: typeof FUNDING;
-}
-
-export function waitForFundingRequest<T extends BaseFundingChannelState>(
+export function waitForFundingRequest<T extends SharedFundingState>(
   params: T,
 ): WaitForFundingRequest {
   return {
+    ...directFundingState(params),
     type: WAIT_FOR_FUNDING_REQUEST,
-    stage: FUNDING,
-    ...channelOpen(params),
-    fundingState: params.fundingState,
-    funded: false,
+    fundingType: UNKNOWN_FUNDING_TYPE,
   };
 }
 
-export function approveFunding<T extends BaseFundingChannelState>(params: T): ApproveFunding {
-  return {
-    type: APPROVE_FUNDING,
-    stage: FUNDING,
-    ...fundingChannelState(params),
-    fundingState: params.fundingState,
-  };
-}
-
-export function waitForFundingAndPostFundSetup<T extends BaseFundingChannelState>(
-  params: T,
-): WaitForFundingAndPostFundSetup {
-  return {
-    type: WAIT_FOR_FUNDING_AND_POST_FUND_SETUP,
-    stage: FUNDING,
-    ...fundingChannelState(params),
-  };
-}
-
-export function waitForFundingConfirmation<T extends BaseFundingChannelState>(
-  params: T,
-): WaitForFundingConfirmation {
-  return { type: WAIT_FOR_FUNDING_CONFIRMATION, stage: FUNDING, ...fundingChannelState(params) };
-}
-
-export function aWaitForPostFundSetup<T extends BaseFundingChannelState>(
-  params: T,
-): AWaitForPostFundSetup {
-  return { type: A_WAIT_FOR_POST_FUND_SETUP, stage: FUNDING, ...fundingChannelState(params) };
-}
-
-export function bWaitForPostFundSetup<T extends BaseFundingChannelState>(
-  params: T,
-): BWaitForPostFundSetup {
-  return { type: B_WAIT_FOR_POST_FUND_SETUP, stage: FUNDING, ...fundingChannelState(params) };
-}
-
-export function acknowledgeFundingSuccess<T extends BaseFundingChannelState>(
-  params: T,
-): AcknowledgeFundingSuccess {
-  return { type: ACKNOWLEDGE_FUNDING_SUCCESS, stage: FUNDING, ...fundingChannelState(params) };
-}
-
-export function sendFundingDeclinedMessage<T extends BaseFundingChannelState>(
-  params: T,
-): SendFundingDeclinedMessage {
-  return { type: SEND_FUNDING_DECLINED_MESSAGE, stage: FUNDING, ...fundingChannelState(params) };
-}
-
-export function acknowledgeFundingDeclined<T extends BaseFundingChannelState>(
-  params: T,
-): AcknowledgeFundingDeclined {
-  return { type: ACKNOWLEDGE_FUNDING_DECLINED, stage: FUNDING, ...fundingChannelState(params) };
-}
-
-export type FundingChannelState =
-  | WaitForFundingRequest
-  | ApproveFunding
-  | WaitForFundingAndPostFundSetup
-  | WaitForFundingConfirmation
-  // ^^^ PlayerA should never let PlayerB get into this state, as it lets PlayerB move to the application phase with no real value at stake, giving them a chance to take the real value that's backing PlayerA's stake.
-  | AWaitForPostFundSetup
-  | BWaitForPostFundSetup
-  | AcknowledgeFundingSuccess // <-- at this point, a message might need to be sent
-  | SendFundingDeclinedMessage
-  | AcknowledgeFundingDeclined;
-
-export type FundingState = DirectFundingState;
-
-/**
- * Question: Is it true that there is exactly one state S such that player B should send
- * their post fund setup upon reaching S?
- *
- * Answer: Yes, and that is whatever you call AcknowledgeFundingSuccess
- */
+export * from './directFunding';
+export type FundingState = WaitForFundingRequest | DirectFundingState;
+export { SharedFundingState };
