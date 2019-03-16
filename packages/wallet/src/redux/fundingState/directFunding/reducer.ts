2e342bbc57f1e3f4eff3675d0563c53790a145c6
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-16 12:09:06 -0600

    Update directFundingStateReducer
    It is now only responsible for updating the funding state when the
    funding type is known to be DIRECT_FUNDING_TYPE

--- packages/wallet/src/redux/fundingState/directFunding/reducer.ts
+++ packages/wallet/src/redux/fundingState/directFunding/reducer.ts
@@ -6,19 +6,16 @@ import { createDepositTransaction } from '../../../utils/transaction-generator';
 
 import { bigNumberify } from 'ethers/utils';
 import { DIRECT_FUNDING } from '../state';
+import { StateWithSideEffects } from 'src/redux/shared/state';
 
 export const directFundingStateReducer = (
-  state: states.WaitForFundingRequest | states.DirectFundingState,
+  state: states.DirectFundingState,
   action: actions.WalletAction,
-  channelId: string,
-  ourIndex: number,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (state.type) {
     //
-    case states.WAIT_FOR_FUNDING_REQUEST:
-      return waitForFundingRequestReducer(state, action);
     case states.WAIT_FOR_FUNDING_APPROVAL:
-      return waitForFundingApprovalReducer(state, action, ourIndex, channelId);
+      return waitForFundingApprovalReducer(state, action);
     case states.A_WAIT_FOR_DEPOSIT_TO_BE_SENT_TO_METAMASK:
       return aWaitForDepositToBeSentToMetaMaskReducer(state, action);
     case states.A_SUBMIT_DEPOSIT_IN_METAMASK:
@@ -26,10 +23,10 @@ export const directFundingStateReducer = (
     case states.A_WAIT_FOR_DEPOSIT_CONFIRMATION:
       return aWaitForDepositConfirmationReducer(state, action);
     case states.A_WAIT_FOR_OPPONENT_DEPOSIT:
-      return aWaitForOpponentDepositReducer(state, action, channelId);
+      return aWaitForOpponentDepositReducer(state, action);
     //
     case states.B_WAIT_FOR_OPPONENT_DEPOSIT:
-      return bWaitForOpponentDepositReducer(state, action, channelId);
+      return bWaitForOpponentDepositReducer(state, action);
     case states.B_WAIT_FOR_DEPOSIT_TO_BE_SENT_TO_METAMASK:
       return bWaitForDepositToBeSentToMetaMaskReducer(state, action);
     case states.B_SUBMIT_DEPOSIT_IN_METAMASK:
@@ -38,252 +35,241 @@ export const directFundingStateReducer = (
       return bWaitForDepositConfirmationReducer(state, action);
     //
     case states.A_DEPOSIT_TRANSACTION_FAILED:
-      return aDepositTransactionFailedReducer(state, action, channelId);
+      return aDepositTransactionFailedReducer(state, action);
     case states.B_DEPOSIT_TRANSACTION_FAILED:
-      return bDepositTransactionFailedReducer(state, action, channelId);
+      return bDepositTransactionFailedReducer(state, action);
     case states.FUNDING_CONFIRMED:
-      return { fundingState: state };
+      return { state };
     default:
       return unreachable(state);
-  }
-};
-
-const waitForFundingRequestReducer = (
-  state: states.WaitForFundingRequest,
-  action: actions.WalletAction,
-): states.DirectFundingStateWithSideEffects => {
-  switch (action.type) {
-    case actions.FUNDING_REQUESTED:
-      return {
-        fundingState: states.waitForFundingApproval({
-          ...state,
-          fundingType: DIRECT_FUNDING,
-        }),
-      };
-    default:
-      return { fundingState: state };
   }
 };
 
 const waitForFundingApprovalReducer = (
   state: states.WaitForFundingApproval,
   action: actions.WalletAction,
-  ourIndex: number,
-  channelId: string,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.FUNDING_APPROVED:
-      if (ourIndex === 0) {
+      if (state.ourIndex === 0) {
         return {
-          fundingState: states.aWaitForDepositToBeSentToMetaMask({
+          state: states.aWaitForDepositToBeSentToMetaMask({
             ...state,
             fundingType: DIRECT_FUNDING,
           }),
           outboxState: {
-            transactionOutbox: createDepositTransaction(channelId, state.requestedYourContribution),
+            transactionOutbox: createDepositTransaction(
+              state.channelId,
+              state.requestedYourContribution,
+            ),
           },
         };
       } else {
         return {
-          fundingState: states.bWaitForOpponentDeposit({
+          state: states.bWaitForOpponentDeposit({
             ...state,
             fundingType: DIRECT_FUNDING,
           }),
         };
       }
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
 const aWaitForDepositToBeSentToMetaMaskReducer = (
   state: states.AWaitForDepositToBeSentToMetaMask,
   action: actions.WalletAction,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.TRANSACTION_SENT_TO_METAMASK:
-      return { fundingState: states.aSubmitDepositInMetaMask(state) };
+      return { state: states.aSubmitDepositInMetaMask(state) };
     case actions.FUNDING_RECEIVED_EVENT:
       return {
-        fundingState: states.aWaitForDepositToBeSentToMetaMask({
+        state: states.aWaitForDepositToBeSentToMetaMask({
           ...state,
         }),
       };
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
 const aSubmitDepositToMetaMaskReducer = (
   state: states.ASubmitDepositInMetaMask,
   action: actions.WalletAction,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.FUNDING_RECEIVED_EVENT:
       return {
-        fundingState: states.aSubmitDepositInMetaMask({ ...state }),
+        state: states.aSubmitDepositInMetaMask({ ...state }),
       };
     case actions.TRANSACTION_SUBMITTED:
       return {
-        fundingState: states.aWaitForDepositConfirmation({
+        state: states.aWaitForDepositConfirmation({
           ...state,
           transactionHash: action.transactionHash,
         }),
       };
     case actions.TRANSACTION_SUBMISSION_FAILED:
-      return { fundingState: states.aDepositTransactionFailed(state) };
+      return { state: states.aDepositTransactionFailed(state) };
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
 const aWaitForDepositConfirmationReducer = (
   state: states.AWaitForDepositConfirmation,
   action: actions.WalletAction,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.FUNDING_RECEIVED_EVENT:
       return {
-        fundingState: states.aWaitForDepositConfirmation({ ...state }),
+        state: states.aWaitForDepositConfirmation({ ...state }),
       };
     case actions.TRANSACTION_CONFIRMED:
-      return { fundingState: states.aWaitForOpponentDeposit(state) };
+      return { state: states.aWaitForOpponentDeposit(state) };
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
 const aWaitForOpponentDepositReducer = (
   state: states.AWaitForOpponentDeposit,
   action: actions.WalletAction,
-  channelId: string,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.FUNDING_RECEIVED_EVENT:
-      if (sufficientlyFundedToProgress('A', state, action, channelId)) {
-        return { fundingState: states.fundingConfirmed(state) };
+      if (sufficientlyFundedToProgress('A', state, action)) {
+        return { state: states.fundingConfirmed(state) };
       } else {
-        return { fundingState: state };
+        return { state };
       }
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
 const bWaitForOpponentDepositReducer = (
   state: states.BWaitForOpponentDeposit,
   action: actions.WalletAction,
-  channelId: string,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.FUNDING_RECEIVED_EVENT:
-      if (channelId !== action.destination) {
-        return { fundingState: state };
+      if (state.channelId !== action.destination) {
+        return { state };
       }
       if (
         bigNumberify(action.totalForDestination).lt(
           bigNumberify(state.requestedTotalFunds).sub(state.requestedYourContribution),
         )
       ) {
-        return { fundingState: state };
+        return { state };
       }
 
       return {
-        fundingState: states.bWaitForDepositToBeSentToMetaMask({ ...state }),
+        state: states.bWaitForDepositToBeSentToMetaMask({ ...state }),
         outboxState: {
-          transactionOutbox: createDepositTransaction(channelId, state.requestedYourContribution),
+          transactionOutbox: createDepositTransaction(
+            state.channelId,
+            state.requestedYourContribution,
+          ),
         },
       };
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
 const bWaitForDepositToBeSentToMetaMaskReducer = (
   state: states.BWaitForDepositToBeSentToMetaMask,
   action: actions.WalletAction,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.TRANSACTION_SENT_TO_METAMASK:
-      return { fundingState: states.bSubmitDepositInMetaMask(state) };
+      return { state: states.bSubmitDepositInMetaMask(state) };
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
 const bSubmitDepositInMetaMaskReducer = (
   state: states.BSubmitDepositInMetaMask,
   action: actions.WalletAction,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     // This case should not happen in theory, but it does in practice.
     // B submits deposit transaction, transaction is confirmed, A sends postfundset, B receives postfundsetup
     // All of the above happens before B receives transaction submitted
     case actions.TRANSACTION_SUBMITTED:
       return {
-        fundingState: states.bWaitForDepositConfirmation({
+        state: states.bWaitForDepositConfirmation({
           ...state,
           transactionHash: action.transactionHash,
         }),
       };
     case actions.TRANSACTION_SUBMISSION_FAILED:
-      return { fundingState: states.bDepositTransactionFailed(state) };
+      return { state: states.bDepositTransactionFailed(state) };
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
 const aDepositTransactionFailedReducer = (
   state: states.ADepositTransactionFailed,
   action: actions.WalletAction,
-  channelId: string,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.RETRY_TRANSACTION:
       return {
-        fundingState: states.aWaitForDepositToBeSentToMetaMask({
+        state: states.aWaitForDepositToBeSentToMetaMask({
           ...state,
         }),
         outboxState: {
-          transactionOutbox: createDepositTransaction(channelId, state.requestedYourContribution),
+          transactionOutbox: createDepositTransaction(
+            state.channelId,
+            state.requestedYourContribution,
+          ),
         },
       };
   }
-  return { fundingState: state };
+  return { state };
 };
 
 const bDepositTransactionFailedReducer = (
   state: states.BDepositTransactionFailed,
   action: actions.WalletAction,
-  channelId: string,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.RETRY_TRANSACTION:
       return {
-        fundingState: states.bWaitForDepositToBeSentToMetaMask({ ...state }),
+        state: states.bWaitForDepositToBeSentToMetaMask({ ...state }),
         outboxState: {
-          transactionOutbox: createDepositTransaction(channelId, state.requestedYourContribution),
+          transactionOutbox: createDepositTransaction(
+            state.channelId,
+            state.requestedYourContribution,
+          ),
         },
       };
   }
-  return { fundingState: state };
+  return { state };
 };
 
 const bWaitForDepositConfirmationReducer = (
   state: states.BWaitForDepositConfirmation,
   action: actions.WalletAction,
-): states.DirectFundingStateWithSideEffects => {
+): StateWithSideEffects<states.DirectFundingState> => {
   switch (action.type) {
     case actions.FUNDING_RECEIVED_EVENT:
     case actions.TRANSACTION_CONFIRMED:
-      return { fundingState: states.fundingConfirmed(state) };
+      return { state: states.fundingConfirmed(state) };
     default:
-      return { fundingState: state };
+      return { state };
   }
 };
 
-function sufficientlyFundedToProgress(player: 'A' | 'B', state, action, channelId) {
-  if (channelId !== action.destination) {
+function sufficientlyFundedToProgress(player: 'A' | 'B', state, action) {
+  if (state.channelId !== action.destination) {
     return false;
   }
   if (player === 'A' && bigNumberify(action.totalForDestination).gte(state.requestedTotalFunds)) {
