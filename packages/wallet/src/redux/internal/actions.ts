dcfc5d70696dd7fe3fa71ae19da5e3da7d6adc57
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-17 14:37:52 -0600

    Consistent naming

--- packages/wallet/src/redux/internal/actions.ts
+++ packages/wallet/src/redux/internal/actions.ts
@@ -1,14 +1,14 @@
 export const DIRECT_FUNDING_REQUESTED = 'WALLET.INTERNAL.DIRECT_FUNDING_REQUESTED';
 export const directFundingRequested = (
   channelId: string,
-  safeToDepositAmount: string,
+  safeToDepositLevel: string,
   totalFundingRequired: string,
   requiredDeposit: string,
 ) => ({
   type: DIRECT_FUNDING_REQUESTED as typeof DIRECT_FUNDING_REQUESTED,
   channelId,
   totalFundingRequired,
-  safeToDepositAmount,
+  safeToDepositLevel,
   requiredDeposit,
 });
 export type DirectFundingRequested = ReturnType<typeof directFundingRequested>;
