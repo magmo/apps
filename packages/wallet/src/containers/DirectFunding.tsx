0dbc6cff3179ae3182e2d007cb7c13b4675d7f27
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-18 12:01:34 -0600

    DirectFunding container is player-agnostic

--- packages/wallet/src/containers/DirectFunding.tsx
+++ packages/wallet/src/containers/DirectFunding.tsx
@@ -6,13 +6,13 @@ import * as fundingStates from '../redux/fundingState/state';
 import * as actions from '../redux/actions';
 
 import { unreachable } from '../utils/reducer-utils';
-import TransactionFailed from '../components/TransactionFailed';
 import ApproveFunding from '../components/funding/ApproveFunding';
-import { AFundingStep, BFundingStep } from '../components/funding/FundingStep';
+import { FundingStep } from '../components/funding/FundingStep';
 import EtherscanLink from '../components/EtherscanLink';
+import TransactionFailed from '../components/TransactionFailed';
 
 interface Props {
-  state: fundingStates.FundingState;
+  state: fundingStates.DirectFundingState;
   fundingApproved: () => void;
   fundingRejected: () => void;
   fundingSuccessAcknowledged: () => void;
@@ -23,68 +23,51 @@ interface Props {
 class DirectFundingContainer extends PureComponent<Props> {
   render() {
     const { state, fundingApproved, fundingRejected, retryTransactionAction } = this.props;
-
-    switch (state.type) {
-      case fundingStates.WAIT_FOR_FUNDING_REQUEST:
-        return null;
-      case fundingStates.WAIT_FOR_FUNDING_APPROVAL:
-        return (
-          <ApproveFunding
-            fundingApproved={fundingApproved}
-            fundingRejected={fundingRejected}
-            requestedTotalFunds={state.requestedTotalFunds}
-            requestedYourContribution={state.requestedYourContribution}
-          />
-        );
-
-      // PlayerA
-      case fundingStates.A_WAIT_FOR_DEPOSIT_TO_BE_SENT_TO_METAMASK:
-        return <AFundingStep step={0} />;
-      case fundingStates.A_SUBMIT_DEPOSIT_IN_METAMASK:
-        return <AFundingStep step={1}>Please confirm the transaction in MetaMask!</AFundingStep>;
-      case fundingStates.A_WAIT_FOR_DEPOSIT_CONFIRMATION:
-        return (
-          <AFundingStep step={2}>
-            Check the progress on&nbsp;
-            <EtherscanLink
-              transactionID={state.transactionHash}
-              networkId={-1} // TODO: Fix network id
-              title="Etherscan"
-            />
-            !
-          </AFundingStep>
-        );
-      case fundingStates.A_WAIT_FOR_OPPONENT_DEPOSIT:
-        return <AFundingStep step={3} />;
-
-      // PlayerB
-      case fundingStates.B_WAIT_FOR_OPPONENT_DEPOSIT:
-        return <BFundingStep step={1} />;
-      case fundingStates.B_WAIT_FOR_DEPOSIT_TO_BE_SENT_TO_METAMASK:
-        return <BFundingStep step={2} />;
-      case fundingStates.B_SUBMIT_DEPOSIT_IN_METAMASK:
-        return <BFundingStep step={2}> Please confirm the transaction in MetaMask!</BFundingStep>;
-      case fundingStates.B_WAIT_FOR_DEPOSIT_CONFIRMATION:
-        return (
-          <BFundingStep step={3}>
-            Check the progress on&nbsp;
-            <EtherscanLink
-              transactionID={state.transactionHash}
-              networkId={-1} // TODO: Fix the network id
-              title="Etherscan"
-            />
-            !
-          </BFundingStep>
-        );
-      case fundingStates.A_DEPOSIT_TRANSACTION_FAILED:
-        return <TransactionFailed name="deposit" retryAction={retryTransactionAction} />;
-      case fundingStates.B_DEPOSIT_TRANSACTION_FAILED:
-        return <TransactionFailed name="deposit" retryAction={retryTransactionAction} />;
-      case fundingStates.FUNDING_CONFIRMED:
-        return null;
-      default:
-        return unreachable(state);
+    if (fundingStates.stateIsWaitForFundingApproval(state)) {
+      return (
+        <ApproveFunding
+          fundingApproved={fundingApproved}
+          fundingRejected={fundingRejected}
+          requestedTotalFunds={state.requestedTotalFunds}
+          requestedYourContribution={state.requestedYourContribution}
+        />
+      );
     }
+    if (
+      fundingStates.stateIsNotSafeToDeposit(state) ||
+      fundingStates.stateIsWaitForFundingConfirmation(state)
+    ) {
+      return <FundingStep state={state} />;
+    }
+    if (fundingStates.stateIsDepositing(state)) {
+      switch (state.depositStatus) {
+        case fundingStates.depositing.WAIT_FOR_TRANSACTION_SENT:
+          return <FundingStep state={state} />;
+        case fundingStates.depositing.WAIT_FOR_DEPOSIT_APPROVAL:
+          return (
+            <FundingStep state={state}>Please confirm the transaction in MetaMask!</FundingStep>
+          );
+        case fundingStates.depositing.WAIT_FOR_DEPOSIT_CONFIRMATION:
+          return (
+            <FundingStep state={state}>
+              Check the progress on&nbsp;
+              <EtherscanLink
+                transactionID={state.transactionHash}
+                networkId={-1} // TODO: Fix network id
+                title="Etherscan"
+              />
+              !
+            </FundingStep>
+          );
+        case fundingStates.depositing.DEPOSIT_TRANSACTION_FAILED:
+          return <TransactionFailed name="deposit" retryAction={retryTransactionAction} />;
+      }
+    }
+    if (fundingStates.stateIsChannelFunded(state)) {
+      return null;
+    }
+
+    return unreachable(state);
   }
 }
 
