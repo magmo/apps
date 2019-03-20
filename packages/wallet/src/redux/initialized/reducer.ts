017356bdfd81d37d8a4693d8aa1f5e91aa321d07
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-20 12:27:08 -0600

    initialized reducer does not erase old outboxState items

--- packages/wallet/src/redux/initialized/reducer.ts
+++ packages/wallet/src/redux/initialized/reducer.ts
@@ -4,13 +4,16 @@ import { WalletAction } from '../actions';
 import { combineReducersWithSideEffects } from '../../utils/reducer-utils';
 import { channelStateReducer } from '../channelState/reducer';
 import { fundingStateReducer } from '../fundingState/reducer';
+import { applySideEffects } from '../outbox';
 
 export function initializedReducer(
   state: InitializedState,
   action: WalletAction,
 ): InitializedState {
-  const { state: newState, outboxState } = combinedReducer(state, action);
-  return { ...state, ...newState, outboxState };
+  const { state: newState, outboxState: sideEffects } = combinedReducer(state, action);
+  // Since the wallet state itself has an outbox state, we need to apply the side effects
+  // by hand.
+  return { ...state, ...newState, outboxState: applySideEffects(state.outboxState, sideEffects) };
 }
 const combinedReducer = combineReducersWithSideEffects({
   channelState: channelStateReducer,
