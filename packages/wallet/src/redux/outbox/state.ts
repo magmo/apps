5861382f0b0401757e7eab92cc9948c8eb8a7ad3
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-22 10:40:39 -0600

    Define SideEffects type
    OutboxState is planned to have no optional fields. This type allows
    a reducer-with-side-effects to not have to return a full OutboxState,
    but instead return any object that can be used to accumulate side
    effects in an OutboxState.

--- packages/wallet/src/redux/outbox/state.ts
+++ packages/wallet/src/redux/outbox/state.ts
@@ -2,9 +2,18 @@ import { TransactionRequest } from 'ethers/providers';
 import { WalletEvent, DisplayAction } from 'magmo-wallet-client';
 import { internal } from '../actions';
 
+export const EMPTY_OUTBOX_STATE: OutboxState = {
+  displayOutbox: undefined,
+  messageOutbox: undefined,
+  transactionOutbox: undefined,
+  actionOutbox: undefined,
+};
+
 export interface OutboxState {
   displayOutbox?: DisplayAction;
   messageOutbox?: WalletEvent;
   transactionOutbox?: TransactionRequest;
   actionOutbox?: internal.InternalAction;
 }
+
+export type SideEffects = Partial<OutboxState> | undefined;
