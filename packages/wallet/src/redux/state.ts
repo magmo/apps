2671222571682c210886960e28af33db18a8d682
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-28 12:12:35 -0600

    Add getChannelStatus selector

--- packages/wallet/src/redux/state.ts
+++ packages/wallet/src/redux/state.ts
@@ -1,6 +1,7 @@
 import { SharedWalletState, emptyState } from './shared/state';
 import { InitializingState } from './initializing/state';
 import { InitializedState } from './initialized/state';
+import { ChannelStatus } from './channelState/state';
 
 export * from './initialized/state';
 export * from './initializing/state';
@@ -8,3 +9,7 @@ export * from './initializing/state';
 export { SharedWalletState, emptyState };
 
 export type WalletState = InitializingState | InitializedState;
+
+export function getChannelStatus(state: WalletState, channelId: string): ChannelStatus {
+  return state.channelState.initializedChannels[channelId];
+}
