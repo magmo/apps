4dd04997138984d7c5d9166300a5698d9a8570e5
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-28 11:40:37 -0600

    Remove actionOutbox

--- packages/wallet/src/redux/initialized/reducer.ts
+++ packages/wallet/src/redux/initialized/reducer.ts
@@ -10,16 +10,24 @@ export function initializedReducer(
   state: InitializedState,
   action: WalletAction,
 ): InitializedState {
+  // Apply the "independent" reducer
   const { state: newState, sideEffects } = combinedReducer(state, action);
   // Since the wallet state itself has an outbox state, we need to apply the side effects
   // by hand.
-  return {
+  const updatedState = {
     ...state,
     ...newState,
     outboxState: accumulateSideEffects(state.outboxState, sideEffects),
   };
+
+  // Inspect the global state for interactions
+  return coordinator(updatedState, action);
 }
 const combinedReducer = combineReducersWithSideEffects({
   channelState: channelStateReducer,
   fundingState: fundingStateReducer,
 });
+
+export function coordinator(state: InitializedState, action: WalletAction): InitializedState {
+  return state;
+}
