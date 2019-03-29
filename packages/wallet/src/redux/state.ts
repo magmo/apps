39ca47f450b785934cfc325b3f5e4074953b84ba
Tom Close <tom.close@cantab.net>
2019-03-28 17:39:24 -0700

    Remove unused export

--- packages/wallet/src/redux/state.ts
+++ packages/wallet/src/redux/state.ts
@@ -1,10 +1,7 @@
-import { SharedWalletState, emptyState } from './shared/state';
 import { InitializingState } from './initializing/state';
 import { InitializedState } from './initialized/state';
 
 export * from './initialized/state';
 export * from './initializing/state';
-
-export { SharedWalletState, emptyState };
 
 export type WalletState = InitializingState | InitializedState;
