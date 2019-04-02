51836ecd954f7388e8a76d53326d34974ea8c294
Alex Gap <alex.gap@gmail.com>
2019-04-02 13:13:19 -0700

    Add PlayerIndex, switch to WaitForApproval

--- packages/wallet/src/redux/funding/indirect-funding/player-a/state.ts
+++ packages/wallet/src/redux/funding/indirect-funding/player-a/state.ts
@@ -1,6 +1,7 @@
 import { Properties } from '../../../utils';
+import { PlayerIndex } from '../../../types';
 
-export const WAIT_FOR_STRATEGY_RESPONSE = 'WAIT_FOR_STRATEGY_RESPONSE';
+export const WAIT_FOR_APPROVAL = 'WAIT_FOR_APPROVAL';
 export const WAIT_FOR_PRE_FUND_SETUP_1 = 'WAIT_FOR_PRE_FUND_SETUP_1';
 export const WAIT_FOR_DIRECT_FUNDING = 'WAIT_FOR_DIRECT_FUNDING';
 export const WAIT_FOR_POST_FUND_SETUP_1 = 'WAIT_FOR_POST_FUND_SETUP_1';
@@ -8,65 +9,68 @@ export const WAI_FOR_LEDGER_UPDATE_1 = 'WAI_FOR_LEDGER_UPDATE_1';
 
 interface BasePlayerAState {
   channelId: string;
+  player: PlayerIndex.A;
+}
+
+interface LedgerChannelExists extends BasePlayerAState {
   ledgerId: string;
 }
 
-export interface WaitForStrategyResponse extends BasePlayerAState {
-  type: typeof WAIT_FOR_STRATEGY_RESPONSE;
+export interface WaitForApproval extends BasePlayerAState {
+  type: typeof WAIT_FOR_APPROVAL;
 }
 
-export interface WaitForPreFundSetup1 extends BasePlayerAState {
+export interface WaitForPreFundSetup1 extends LedgerChannelExists {
   type: typeof WAIT_FOR_PRE_FUND_SETUP_1;
 }
 
-export interface WaitForDirectFunding extends BasePlayerAState {
+export interface WaitForDirectFunding extends LedgerChannelExists {
   type: typeof WAIT_FOR_DIRECT_FUNDING;
 }
-export interface WaitForPostFundSetup1 extends BasePlayerAState {
+export interface WaitForPostFundSetup1 extends LedgerChannelExists {
   type: typeof WAIT_FOR_POST_FUND_SETUP_1;
 }
-export interface WaitForLedgerUpdate1 extends BasePlayerAState {
+export interface WaitForLedgerUpdate1 extends LedgerChannelExists {
   type: typeof WAI_FOR_LEDGER_UPDATE_1;
 }
 
 export type PlayerAState =
-  | WaitForStrategyResponse
+  | WaitForApproval
   | WaitForPreFundSetup1
   | WaitForDirectFunding
   | WaitForPostFundSetup1
   | WaitForLedgerUpdate1;
 
-export function waitForStrategyResponse(
-  params: Properties<WaitForStrategyResponse>,
-): WaitForStrategyResponse {
-  const { channelId, ledgerId } = params;
-  return { type: WAIT_FOR_STRATEGY_RESPONSE, channelId, ledgerId };
+export function waitForApproval(params: Properties<WaitForApproval>): WaitForApproval {
+  const { channelId } = params;
+
+  return { type: WAIT_FOR_APPROVAL, player: PlayerIndex.A, channelId };
 }
 
 export function waitForPreFundSetup1(
   params: Properties<WaitForPreFundSetup1>,
 ): WaitForPreFundSetup1 {
   const { channelId, ledgerId } = params;
-  return { type: WAIT_FOR_PRE_FUND_SETUP_1, channelId, ledgerId };
+  return { type: WAIT_FOR_PRE_FUND_SETUP_1, player: PlayerIndex.A, channelId, ledgerId };
 }
 
 export function waitForDirectFunding(
   params: Properties<WaitForDirectFunding>,
 ): WaitForDirectFunding {
   const { channelId, ledgerId } = params;
-  return { type: WAIT_FOR_DIRECT_FUNDING, channelId, ledgerId };
+  return { type: WAIT_FOR_DIRECT_FUNDING, player: PlayerIndex.A, channelId, ledgerId };
 }
 
 export function waitForPostFundSetup1(
   params: Properties<WaitForPostFundSetup1>,
 ): WaitForPostFundSetup1 {
   const { channelId, ledgerId } = params;
-  return { type: WAIT_FOR_POST_FUND_SETUP_1, channelId, ledgerId };
+  return { type: WAIT_FOR_POST_FUND_SETUP_1, player: PlayerIndex.A, channelId, ledgerId };
 }
 
 export function waitForLedgerUpdate1(
   params: Properties<WaitForLedgerUpdate1>,
 ): WaitForLedgerUpdate1 {
   const { channelId, ledgerId } = params;
-  return { type: WAI_FOR_LEDGER_UPDATE_1, channelId, ledgerId };
+  return { type: WAI_FOR_LEDGER_UPDATE_1, player: PlayerIndex.A, channelId, ledgerId };
 }
