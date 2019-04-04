a43d88fca87bf280ada5f09c9361dec6ec8eb08e
George Knee <georgeknee@googlemail.com>
2019-04-04 14:23:07 +0100

    fix merge conflicts

--- packages/wallet/src/containers/initialized.tsx
+++ packages/wallet/src/containers/initialized.tsx
@@ -1,11 +1,7 @@
 import * as states from '../redux/state';
 import React, { PureComponent } from 'react';
-<<<<<<< HEAD:packages/wallet/src/containers/initialized.tsx
-import SidebarLayout from '../components/sidebar-layout';
-=======
 import StatusBarLayout from '../components/status-bar-layout';
 import ChannelRegistryLayout from '../components/channel-registry-layout';
->>>>>>> being work on a single story, including constructing an entire wallet state from basic types; channel registry layout component:packages/wallet/src/containers/Initialized.tsx
 import { connect } from 'react-redux';
 import ChannelContainer from './channel';
 interface Props {
@@ -23,16 +19,10 @@ class WalletInitializedContainer extends PureComponent<Props> {
       );
     } else {
       return (
-<<<<<<< HEAD:packages/wallet/src/containers/initialized.tsx
-        <SidebarLayout>
-          <h1>Wallet initialized</h1>
-        </SidebarLayout>
-=======
         <StatusBarLayout>
           <ChannelRegistryLayout state={state} />
           {/* <span>Wallet initialized</span> */}
         </StatusBarLayout>
->>>>>>> being work on a single story, including constructing an entire wallet state from basic types; channel registry layout component:packages/wallet/src/containers/Initialized.tsx
       );
     }
   }
