1a944c76f4564a2a904706953e4442442c1d6f3d
George Knee <georgeknee@googlemail.com>
2019-04-04 15:45:31 +0100

    twin storybook

--- packages/wallet/src/components/status-bar-layout.tsx
+++ packages/wallet/src/components/status-bar-layout.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
 import { PureComponent } from 'react';
 import NetworkStatus from './network-status';
-import Modal from 'react-modal';
 
-// todo: style to appear from the left as per:
-// https://github.com/DimitryDushkin/sliding-pane/blob/master/src/index.styl
+interface LayoutProps {
+  position: "left" | "center" | "right";
+}
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    width: '320px',
-    height: '450px',
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-};
-
-export default class StatusBarLayout extends PureComponent {
+export default class StatusBarLayout extends PureComponent<LayoutProps> {
   render() {
+
     return (
-      <Modal isOpen={true} style={customStyles} ariaHideApp={false}>
         <div className="d-flex flex-column h-100">
           <NetworkStatus />
 
@@ -38,7 +22,6 @@ export default class StatusBarLayout extends PureComponent {
             <div style={{ color: 'rgba(0, 0, 0, 0.3)' }}>Magmo wallet v0.0.1</div>
           </div>
         </div>
-      </Modal>
     );
   }
 }
