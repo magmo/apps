339e68381e5e6a6fc3df65b4f4ffbf221c5bcdfb
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-04-04 16:48:55 -0600

    Add player-b test case

--- packages/wallet/src/redux/indirect-funding/player-b/__tests__/reducer.test.ts
+++ packages/wallet/src/redux/indirect-funding/player-b/__tests__/reducer.test.ts
@@ -1,9 +1,12 @@
 import * as states from '../state';
+import * as walletStates from '../../../state';
+import * as channelStates from '../../../channel-state/state';
 import * as actions from '../../../actions';
 
 import * as scenarios from '../../../__tests__/test-scenarios';
 import { playerBReducer } from '../reducer';
 import { itTransitionsProcedureToStateType } from '../../../__tests__/helpers';
+import { WalletProcedure } from '../../../types';
 
 const startingIn = type => `starting in ${type}`;
 const whenActionArrives = type => `when ${type} arrives`;
@@ -12,11 +15,19 @@ function itTransitionToStateType(state, type) {
   itTransitionsProcedureToStateType('indirectFunding', state, type);
 }
 
-const { initializedState, channelId } = scenarios;
+const { initializedState, channelId, ledgerCommitments } = scenarios;
+const { preFundCommitment1 } = ledgerCommitments;
 const defaultState = { ...initializedState };
-const startingState = (state: states.PlayerBState) => ({
+const startingState = (
+  state: states.PlayerBState,
+  channelState?: channelStates.OpenedState,
+): walletStates.IndirectFundingOngoing => ({
   ...defaultState,
   indirectFunding: state,
+  channelState: {
+    ...channelStates.EMPTY_CHANNEL_STATE,
+    initializedChannels: channelState ? { [channelState.channelId]: channelState } : {},
+  },
 });
 
 describe(startingIn(states.WAIT_FOR_APPROVAL), () => {
@@ -31,4 +42,14 @@ describe(startingIn(states.WAIT_FOR_APPROVAL), () => {
 
 describe(startingIn(states.WAIT_FOR_PRE_FUND_SETUP_0), () => {
   describe(whenActionArrives(actions.COMMITMENT_RECEIVED), () => {});
+  const state = startingState(states.waitForPreFundSetup0({ channelId }));
+  const action = actions.commitmentReceived(
+    channelId,
+    WalletProcedure.IndirectFunding,
+    preFundCommitment1,
+    'signature',
+  );
+  const updatedState = playerBReducer(state, action);
+
+  itTransitionToStateType(updatedState, states.WAIT_FOR_DIRECT_FUNDING);
 });
