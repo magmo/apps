07cc39d24618ff096b042711703f16154f1280bd
Tom Close <tom.close@cantab.net>
2019-04-05 13:57:04 +0100

    Rewrite closing state creators using Props

--- packages/wallet/src/redux/channel-state/closing/state.ts
+++ packages/wallet/src/redux/channel-state/closing/state.ts
@@ -1,5 +1,5 @@
 import { ChannelOpen, channelOpen, UserAddressExists, userAddressExists } from '../shared/state';
-import { TransactionExists, Properties } from '../../utils';
+import { TransactionExists, Properties as Props } from '../../utils';
 
 // stage
 export const CLOSING = 'CLOSING';
@@ -77,42 +77,44 @@ export interface Finalized extends ChannelOpen {
   stage: typeof CLOSING;
 }
 
-export function approveConclude<T extends ChannelOpen>(params: T): ApproveConclude {
+export function approveConclude(params: Props<ApproveConclude>): ApproveConclude {
   return { type: APPROVE_CONCLUDE, stage: CLOSING, ...channelOpen(params) };
 }
-export function approveCloseOnChain<T extends ChannelOpen>(params: T): ApproveCloseOnChain {
+export function approveCloseOnChain(params: Props<ApproveCloseOnChain>): ApproveCloseOnChain {
   return { type: APPROVE_CLOSE_ON_CHAIN, stage: CLOSING, ...channelOpen(params) };
 }
 
-export function waitForOpponentConclude<T extends ChannelOpen>(params: T): WaitForOpponentConclude {
+export function waitForOpponentConclude(
+  params: Props<WaitForOpponentConclude>,
+): WaitForOpponentConclude {
   return { type: WAIT_FOR_OPPONENT_CONCLUDE, stage: CLOSING, ...channelOpen(params) };
 }
 
-export function acknowledgeCloseSuccess<T extends ChannelOpen>(params: T): AcknowledgeCloseSuccess {
+export function acknowledgeCloseSuccess(
+  params: Props<AcknowledgeCloseSuccess>,
+): AcknowledgeCloseSuccess {
   return { type: ACKNOWLEDGE_CLOSE_SUCCESS, stage: CLOSING, ...channelOpen(params) };
 }
 
-export function acknowledgeClosedOnChain<T extends ChannelOpen>(
-  params: T,
+export function acknowledgeClosedOnChain(
+  params: Props<AcknowledgeClosedOnChain>,
 ): AcknowledgeClosedOnChain {
   return { type: ACKNOWLEDGE_CLOSED_ON_CHAIN, stage: CLOSING, ...channelOpen(params) };
 }
 
-export function waitForCloseInitiation<T extends UserAddressExists>(
-  params: T,
+export function waitForCloseInitiation(
+  params: Props<WaitForCloseInitiation>,
 ): WaitForCloseInitiation {
   return { type: WAIT_FOR_CLOSE_INITIATION, stage: CLOSING, ...userAddressExists(params) };
 }
 
-export function waitForCloseSubmission<T extends UserAddressExists>(
-  params: T,
+export function waitForCloseSubmission(
+  params: Props<WaitForCloseSubmission>,
 ): WaitForCloseSubmission {
   return { type: WAIT_FOR_CLOSE_SUBMISSION, stage: CLOSING, ...userAddressExists(params) };
 }
 
-export function waitForCloseConfirmed<T extends ChannelOpen & TransactionExists>(
-  params: T,
-): WaitForCloseConfirmed {
+export function waitForCloseConfirmed(params: Props<WaitForCloseConfirmed>): WaitForCloseConfirmed {
   return {
     type: WAIT_FOR_CLOSE_CONFIRMED,
     stage: CLOSING,
@@ -121,17 +123,17 @@ export function waitForCloseConfirmed<T extends ChannelOpen & TransactionExists>
   };
 }
 
-export function acknowledgeConclude<T extends ChannelOpen>(params: T): AcknowledgeConclude {
+export function acknowledgeConclude(params: Props<AcknowledgeConclude>): AcknowledgeConclude {
   return { type: ACKNOWLEDGE_CONCLUDE, stage: CLOSING, ...channelOpen(params) };
 }
 
-export function closeTransactionFailed<T extends UserAddressExists>(
-  params: T,
+export function closeTransactionFailed(
+  params: Props<CloseTransactionFailed>,
 ): CloseTransactionFailed {
   return { type: CLOSE_TRANSACTION_FAILED, stage: CLOSING, ...userAddressExists(params) };
 }
 
-export function finalized(params: Properties<Finalized>): Finalized {
+export function finalized(params: Props<Finalized>): Finalized {
   return { type: FINALIZED, stage: CLOSING, ...channelOpen(params) };
 }
 
