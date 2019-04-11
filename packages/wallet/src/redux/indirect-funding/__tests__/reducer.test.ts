7a59a70c72665e83c3d2b163e6685dc29ec2eb6a
Alex Gap <alex.gap@gmail.com>
2019-04-10 21:29:20 -0700

    Update indirectFunding to be a protocol reducer

--- packages/wallet/src/redux/indirect-funding/__tests__/reducer.test.ts
+++ packages/wallet/src/redux/indirect-funding/__tests__/reducer.test.ts
@@ -2,72 +2,77 @@ import * as actions from '../../actions';
 import * as states from '../../state';
 import { indirectFundingReducer } from '../reducer';
 import * as playerA from '../player-a/reducer';
+import * as playerB from '../player-b/reducer';
 
 import * as scenarios from '../../__tests__/test-scenarios';
 import { PlayerIndex } from '../../types';
+import * as indirectFundingStates from '../../indirect-funding/state';
+import { EMPTY_OUTBOX_STATE } from '../../outbox/state';
+import { EMPTY_CHANNEL_STATE } from '../../channel-state/state';
+const { channelId, ledgerChannel } = scenarios;
+const emptySharedData = {
+  outboxState: EMPTY_OUTBOX_STATE,
+  channelState: EMPTY_CHANNEL_STATE,
+};
 
-const { channelId, initializedState } = scenarios;
-const defaultState = { ...initializedState };
+const defaultState = {
+  protocolState: indirectFundingStates.playerB.waitForApproval(channelId),
+  sharedData: emptySharedData,
+};
 
-describe("when the indirectFunding branch doesn't exist", () => {
-  describe('when FUNDING_REQUESTED arrives', () => {
-    it('works as player A', () => {
-      const state = { ...defaultState };
-      const action = actions.indirectFunding.fundingRequested(channelId, PlayerIndex.A);
-      const updatedState = indirectFundingReducer(state, action);
+describe('when FUNDING_REQUESTED arrives', () => {
+  it('works as player A', () => {
+    const action = actions.indirectFunding.fundingRequested(channelId, PlayerIndex.A);
+    const updatedState = indirectFundingReducer(defaultState, action);
 
-      expect(updatedState.indirectFunding).toMatchObject({
-        type: states.indirectFunding.playerA.WAIT_FOR_APPROVAL,
-      });
+    expect(updatedState.protocolState).toMatchObject({
+      type: states.indirectFunding.playerA.WAIT_FOR_APPROVAL,
     });
+  });
 
-    it('works as player B', () => {
-      const state = { ...defaultState };
-      const action = actions.indirectFunding.fundingRequested(channelId, PlayerIndex.B);
-      const updatedState = indirectFundingReducer(state, action);
+  it('works as player B', () => {
+    const action = actions.indirectFunding.fundingRequested(channelId, PlayerIndex.B);
+    const updatedState = indirectFundingReducer(defaultState, action);
 
-      expect(updatedState.indirectFunding).toMatchObject({
-        type: states.indirectFunding.playerB.WAIT_FOR_APPROVAL,
-      });
+    expect(updatedState.protocolState).toMatchObject({
+      type: states.indirectFunding.playerB.WAIT_FOR_APPROVAL,
     });
   });
 });
 
-describe.skip('when the indirectFunding branch exists', () => {
-  describe('when in a player A state', () => {
-    const player = PlayerIndex.A;
-    it('delegates to the playerAReducer', () => {
-      const state = {
-        ...defaultState,
-        indirectFunding: states.indirectFunding.playerA.waitForApproval({ channelId, player }),
-      };
-      const action = actions.indirectFunding.playerA.strategyApproved(
-        channelId,
-        state.consensusLibrary,
-      );
+describe('when in a player A state', () => {
+  const player = PlayerIndex.A;
+  it('delegates to the playerAReducer', () => {
+    const state = {
+      ...defaultState,
+      protocolState: states.indirectFunding.playerA.waitForApproval({ channelId, player }),
+    };
+    const action = actions.indirectFunding.playerA.strategyApproved(
+      channelId,
+      ledgerChannel.channelType,
+    );
 
-      const playerAReducer = jest.fn();
-      Object.defineProperty(playerA, 'playerAReducer', { value: playerAReducer });
+    const playerAReducer = jest.fn();
+    Object.defineProperty(playerA, 'playerAReducer', { value: playerAReducer });
 
-      indirectFundingReducer(state, action);
-      expect(playerAReducer).toHaveBeenCalledWith(state, action);
-    });
+    indirectFundingReducer(state, action);
+    expect(playerAReducer).toHaveBeenCalledWith(state, action);
   });
+});
 
-  describe('when in a player B state', () => {
-    const player = PlayerIndex.B;
-    it('delegates to the playerBReducer', () => {
-      const state = {
-        ...defaultState,
-        indirectFunding: states.indirectFunding.playerB.waitForApproval({ channelId, player }),
-      };
-      const action = actions.indirectFunding.playerB.strategyProposed(channelId);
-      const updatedState = indirectFundingReducer(state, action);
+describe('when in a player B state', () => {
+  const player = PlayerIndex.B;
+  it('delegates to the playerBReducer', () => {
+    const state = {
+      ...defaultState,
+      protocolState: states.indirectFunding.playerB.waitForApproval({ channelId, player }),
+    };
+    const action = actions.indirectFunding.playerB.strategyProposed(channelId);
 
-      expect(updatedState.indirectFunding).toMatchObject({
-        type: states.indirectFunding.playerB.WAIT_FOR_DIRECT_FUNDING,
-        ledgerId: 'ledgerId',
-      });
-    });
+    const playerBReducer = jest.fn();
+    Object.defineProperty(playerB, 'playerBReducer', { value: playerBReducer });
+
+    indirectFundingReducer(state, action);
+    expect(playerBReducer).toHaveBeenCalledWith(state, action);
   });
 });
--- packages/wallet/src/redux/indirect-funding/reducer.ts
+++ packages/wallet/src/redux/indirect-funding/reducer.ts
@@ -1,74 +1,50 @@
-import * as states from '../state';
 import * as indirectFundingState from './state';
 import * as actions from '../actions';
 import { unreachable } from '../../utils/reducer-utils';
 import { PlayerIndex } from '../types';
+import { ProtocolStateWithSharedData, ProtocolReducer } from '../protocols';
+import { playerAReducer } from './player-a/reducer';
+import { playerBReducer } from './player-b/reducer';
 
-export const indirectFundingReducer = (
-  state: states.Initialized,
+export const indirectFundingReducer: ProtocolReducer<indirectFundingState.IndirectFundingState> = (
+  state: ProtocolStateWithSharedData<indirectFundingState.IndirectFundingState>,
   action: actions.indirectFunding.Action,
-): states.Initialized => {
+): ProtocolStateWithSharedData<indirectFundingState.IndirectFundingState> => {
   if (action.type === actions.indirectFunding.FUNDING_REQUESTED) {
     return fundingRequestedReducer(state, action);
   }
-
-  if (!state.indirectFunding) {
-    return state;
-  }
-
-  switch (state.indirectFunding.player) {
+  switch (state.protocolState.player) {
     case PlayerIndex.A:
-      return playerAReducer(state, action);
+      return playerAReducer(
+        state as ProtocolStateWithSharedData<indirectFundingState.playerA.PlayerAState>,
+        action,
+      );
     case PlayerIndex.B:
-      return playerBReducer(state, action);
+      return playerBReducer(
+        state as ProtocolStateWithSharedData<indirectFundingState.playerB.PlayerBState>,
+        action,
+      );
 
     default:
-      return unreachable(state.indirectFunding);
+      return unreachable(state.protocolState);
   }
 };
 
-function playerAReducer(
-  state: states.Initialized,
-  action: actions.indirectFunding.Action,
-): states.Initialized {
-  return {
-    ...state,
-    indirectFunding: states.indirectFunding.playerA.waitForDirectFunding({
-      ledgerId: 'ledgerId',
-      channelId: 'channelId',
-      player: PlayerIndex.A,
-    }),
-  };
-}
-function playerBReducer(
-  state: states.Initialized,
-  action: actions.indirectFunding.Action,
-): states.Initialized {
-  return {
-    ...state,
-    indirectFunding: states.indirectFunding.playerB.waitForDirectFunding({
-      ledgerId: 'ledgerId',
-      channelId: 'channelId',
-      player: PlayerIndex.B,
-    }),
-  };
-}
-
 function fundingRequestedReducer(
-  state: states.Initialized,
+  state: ProtocolStateWithSharedData<indirectFundingState.IndirectFundingState>,
   action: actions.indirectFunding.FundingRequested,
-): states.Initialized {
+): ProtocolStateWithSharedData<indirectFundingState.IndirectFundingState> {
   const { channelId, playerIndex: player } = action;
   switch (player) {
     case PlayerIndex.A:
       return {
         ...state,
-        indirectFunding: indirectFundingState.playerA.waitForApproval({ channelId, player }),
+        protocolState: indirectFundingState.playerA.waitForApproval({ channelId, player }),
       };
     case PlayerIndex.B:
       return {
         ...state,
-        indirectFunding: indirectFundingState.playerB.waitForApproval({ channelId, player }),
+        protocolState: indirectFundingState.playerB.waitForApproval({ channelId, player }),
       };
     default:
       return unreachable(player);
