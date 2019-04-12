de7a81d522c77659058a63689deff6e6e97a523a
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-04-12 10:37:43 -0600

    Remove unused action

--- packages/wallet/src/redux/protocols/indirect-funding/player-b/actions.ts
+++ packages/wallet/src/redux/protocols/indirect-funding/player-b/actions.ts
@@ -1,5 +1,3 @@
-import { Commitment } from 'fmg-core/lib/commitment';
-
 export const STRATEGY_PROPOSED = 'WALLET.INDIRECT_FUNDING.STRATEGY_PROPOSED';
 export const strategyProposed = (channelId: string) => ({
   type: STRATEGY_PROPOSED as typeof STRATEGY_PROPOSED,
@@ -8,13 +6,4 @@ export const strategyProposed = (channelId: string) => ({
 });
 export type StrategyProposed = ReturnType<typeof strategyProposed>;
 
-export const ALLOCATION_CHANGE_REQUESTED = 'WALLET.INDIRECT_FUNDING.ALLOCATION_CHANGE_REQUESTED';
-export const allocationChangeRequested = (channelId: string, commitment: Commitment) => ({
-  type: ALLOCATION_CHANGE_REQUESTED as typeof ALLOCATION_CHANGE_REQUESTED,
-  processId: channelId,
-  channelId,
-  commitment,
-});
-export type AllocationChangeRequested = ReturnType<typeof allocationChangeRequested>;
-
-export type Action = StrategyProposed | AllocationChangeRequested;
+export type Action = StrategyProposed;
