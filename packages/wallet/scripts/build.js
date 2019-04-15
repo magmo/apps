918680590135f6cfb1a8d262a0664882fb4d4b7f
Alex Gap <alex.gap@gmail.com>
2019-04-15 15:34:14 -0700

    Fix build script

--- packages/wallet/scripts/build.js
+++ packages/wallet/scripts/build.js
@@ -25,6 +25,10 @@ const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
+const {
+  parseABI,
+  parseContractAddress,
+} = require('magmo-devtools');
 
 const measureFileSizesBeforeBuild = FileSizeReporter.measureFileSizesBeforeBuild;
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
