eb92a2463ca10552f2bd4305a56c6a4d28923735
Alex Gap <alex.gap@gmail.com>
2019-04-15 12:36:03 -0700

    Fix transaction tests

--- packages/wallet/src/contract-tests/transactions.test.ts
+++ packages/wallet/src/contract-tests/transactions.test.ts
@@ -57,7 +57,6 @@ describe('transactions', () => {
     transactionToSend = { ...transactionToSend, to: ADJUDICATOR_ADDRESS };
     const transactionReceipt = await signer.sendTransaction(transactionToSend);
 
-    saga.next();
     expect(saga.next(transactionReceipt).value).toEqual(
       put(transactionSubmitted(processId, transactionReceipt.hash || '')),
     );
