e6b54ccccf5d8ee854c09796e31f699370feab5a
Alex Gap <alex.gap@gmail.com>
2019-04-16 16:58:25 -0700

    Initial start on reducer

--- packages/wallet/src/redux/protocols/withdrawing/__tests__/reducer.test.ts
+++ packages/wallet/src/redux/protocols/withdrawing/__tests__/reducer.test.ts
@@ -1,23 +1,50 @@
 import * as scenarios from './scenarios';
 import { withdrawalReducer as reducer, initialize } from '../reducer';
 import * as states from '../states';
-import { itSendsThisTransaction } from '../../../__tests__/helpers';
 import * as TransactionGenerator from '../../../../utils/transaction-generator';
-import { mockTransactionOutboxItem } from '../../../__tests__/test-scenarios';
+
+import { SharedData } from '../..';
 
 // Mocks
-const createConcludeAndWithdrawMock = jest.fn().mockReturnValue(mockTransactionOutboxItem);
+const mockTransaction = { to: '0xabc' };
+const createConcludeAndWithdrawMock = jest.fn().mockReturnValue(mockTransaction);
 Object.defineProperty(TransactionGenerator, 'createConcludeAndWithdrawTransaction', {
   value: createConcludeAndWithdrawMock,
 });
 
+// Helpers
+const whenIn = stage => `when in ${stage}`;
+
+const itTransitionsToFailure = (
+  result: { protocolState: states.WithdrawalState },
+  failure: states.Failure,
+) => {
+  it(`transitions to failure with reason ${failure.reason}`, () => {
+    expect(result.protocolState).toMatchObject(failure);
+  });
+};
+const itTransitionsTo = (result: { protocolState: states.WithdrawalState }, type: string) => {
+  it(`transitions to ${type}`, () => {
+    expect(result.protocolState.type).toEqual(type);
+  });
+};
+const itSendsConcludeAndWithdrawTransaction = (result: { sharedData: SharedData }) => {
+  it('sends the conclude and withdraw transaction', () => {
+    // TODO: This is painful :()
+    expect(result.sharedData.outboxState.transactionOutbox[0]).toMatchObject({
+      transactionRequest: mockTransaction,
+    });
+  });
+};
+
+// Scenario tests
 describe('happy-path scenario', () => {
   const scenario = scenarios.happyPath;
   const { sharedData } = scenario;
 
   describe('when initializing', () => {
-    const { processId, withdrawalAmount } = scenario;
-    const result = initialize(withdrawalAmount, processId, sharedData);
+    const { processId, withdrawalAmount, channelId } = scenario;
+    const result = initialize(withdrawalAmount, channelId, processId, sharedData);
 
     itTransitionsTo(result, states.WAIT_FOR_APPROVAL);
   });
@@ -28,7 +55,7 @@ describe('happy-path scenario', () => {
     const result = reducer(state, sharedData, action);
 
     itTransitionsTo(result, states.WAIT_FOR_TRANSACTION);
-    itSendsThisTransaction(result, mockTransactionOutboxItem);
+    itSendsConcludeAndWithdrawTransaction(result);
   });
 
   describe(whenIn(states.WAIT_FOR_TRANSACTION), () => {
@@ -84,19 +111,3 @@ describe('channel not closed scenario', () => {
     itTransitionsToFailure(result, scenario.channelNotClosedFailure);
   });
 });
-
-const whenIn = stage => `when in ${stage}`;
-
-const itTransitionsToFailure = (
-  result: { protocolState: states.WithdrawalState },
-  failure: states.Failure,
-) => {
-  it(`transitions to failure with reason ${failure.reason}`, () => {
-    expect(result.protocolState).toMatchObject(failure);
-  });
-};
-const itTransitionsTo = (result: { protocolState: states.WithdrawalState }, type: string) => {
-  it(`transitions to ${type}`, () => {
-    expect(result.protocolState.type).toEqual(type);
-  });
-};
--- packages/wallet/src/redux/protocols/withdrawing/reducer.ts
+++ packages/wallet/src/redux/protocols/withdrawing/reducer.ts
@@ -1,22 +1,130 @@
 import { SharedData, ProtocolStateWithSharedData } from '..';
 import * as states from './states';
+import * as actions from './actions';
 import { WithdrawalAction } from './actions';
+import * as selectors from '../../selectors';
+import { CommitmentType } from 'fmg-core/lib/commitment';
+import {
+  createConcludeAndWithdrawTransaction,
+  ConcludeAndWithdrawArgs,
+} from '../../../utils/transaction-generator';
+import { signVerificationData } from '../../../utils/signing-utils';
+import { TransactionRequest } from 'ethers/providers';
+import { initialize as initTransactionState } from '../transaction-submission/reducer';
 
-export function initialize(
+export const initialize = (
   withdrawalAmount: string,
+  channelId: string,
   processId: string,
   sharedData: SharedData,
-): ProtocolStateWithSharedData<states.WithdrawalState> {
+): ProtocolStateWithSharedData<states.WithdrawalState> => {
   return {
-    protocolState: states.waitForApproval({ withdrawalAmount, processId }),
+    protocolState: states.waitForApproval({ withdrawalAmount, processId, channelId }),
     sharedData,
   };
-}
+};
 
-export function withdrawalReducer(
+export const withdrawalReducer = (
   protocolState: states.WithdrawalState,
   sharedData: SharedData,
   action: WithdrawalAction,
-): ProtocolStateWithSharedData<states.WithdrawalState> {
-  return { protocolState, sharedData };
-}
+): ProtocolStateWithSharedData<states.WithdrawalState> => {
+  switch (protocolState.type) {
+    case states.WAIT_FOR_APPROVAL:
+      return waitForApprovalReducer(protocolState, sharedData, action);
+    default:
+      return { protocolState, sharedData };
+    // TODO: unreachable(protocolState);
+  }
+};
+
+const waitForApprovalReducer = (
+  protocolState: states.WaitForApproval,
+  sharedData: SharedData,
+  action: WithdrawalAction,
+): ProtocolStateWithSharedData<states.WithdrawalState> => {
+  switch (action.type) {
+    case actions.WITHDRAWAL_APPROVED:
+      if (!channelIsClosed(protocolState.channelId, sharedData)) {
+        return {
+          protocolState: states.failure(states.FAILURE_REASONS.CHANNEL_NOT_CLOSED),
+          sharedData,
+        };
+      }
+      const { channelId, withdrawalAmount, processId } = protocolState;
+      const { withdrawalAddress } = action;
+      const transaction = createConcludeAndWithTransaction(
+        channelId,
+        withdrawalAmount,
+        withdrawalAddress,
+        sharedData,
+      );
+      const { storage: newSharedData, state: transactionSubmissionState } = initTransactionState(
+        transaction,
+        processId,
+        sharedData,
+      );
+
+      return {
+        protocolState: states.waitForTransaction({
+          ...protocolState,
+          withdrawalAddress,
+          transactionSubmissionState,
+        }),
+        sharedData: newSharedData,
+      };
+    case actions.WITHDRAWAL_REJECTED:
+      return {
+        protocolState: states.failure(states.FAILURE_REASONS.USER_REJECTED),
+        sharedData,
+      };
+    default:
+      return { protocolState, sharedData };
+  }
+};
+
+const channelIsClosed = (channelId: string, sharedData: SharedData): boolean => {
+  const channelState = selectors.getOpenedChannelState(sharedData, channelId);
+  states;
+  const { lastCommitment, penultimateCommitment } = channelState;
+  return (
+    lastCommitment.commitment.commitmentType === CommitmentType.Conclude &&
+    penultimateCommitment.commitment.commitmentType === CommitmentType.Conclude
+  );
+  // TODO: Check if there is a finalized outcome on chain
+};
+
+const createConcludeAndWithTransaction = (
+  channelId: string,
+  withdrawalAmount: string,
+  withdrawalAddress: string,
+  sharedData: SharedData,
+): TransactionRequest => {
+  const channelState = selectors.getOpenedChannelState(sharedData, channelId);
+  const {
+    lastCommitment,
+    penultimateCommitment,
+    participants,
+    ourIndex,
+    privateKey,
+  } = channelState;
+  const participant = participants[ourIndex];
+  const verificationSignature = signVerificationData(
+    participant,
+    withdrawalAddress,
+    withdrawalAmount,
+    withdrawalAddress,
+    privateKey,
+  );
+  const args: ConcludeAndWithdrawArgs = {
+    fromCommitment: penultimateCommitment.commitment,
+    fromSignature: penultimateCommitment.signature,
+    toCommitment: lastCommitment.commitment,
+    toSignature: lastCommitment.signature,
+    participant,
+    amount: withdrawalAmount,
+    destination: withdrawalAddress,
+    verificationSignature,
+  };
+  return createConcludeAndWithdrawTransaction(args);
+};
