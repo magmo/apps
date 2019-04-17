f22b160cbc8af78d8cd1320a172a9dd13f4272db
Mike Kerzhner <mikerzhner@gmail.com>
2019-04-16 17:06:42 -0700

    Cleaning up top level indirect funding stories

--- packages/wallet/src/__stories__/index.tsx
+++ packages/wallet/src/__stories__/index.tsx
@@ -3,11 +3,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import WalletContainer from '../containers/wallet';
 import '../index.scss';
-import * as indirectFundingPlayerA from '../redux/protocols/indirect-funding/player-a/state';
-import * as indirectFundingPlayerB from '../redux/protocols/indirect-funding/player-b/state';
-import * as walletStates from '../redux/state';
-import { indirectFundingWalletState as indirectFundingWalletStateA } from './a-dummy-wallet-states';
-import { indirectFundingWalletState as indirectFundingWalletStateB } from './b-dummy-wallet-states';
 import { dummyWaitForLogin, dummyWaitForMetaMask } from './dummy-wallet-states';
 
 const fakeStore = state => ({
@@ -32,32 +27,6 @@ const walletStateRender = state => () => {
   );
 };
 
-const twinWalletStateRender = (
-  aState: walletStates.Initialized,
-  bState: walletStates.Initialized,
-) => () => {
-  return (
-    <div>
-      Player A x Player B
-      <Provider store={fakeStore(aState)}>
-        <WalletContainer position="left" />
-      </Provider>
-      <Provider store={fakeStore(bState)}>
-        <WalletContainer position="right" />
-      </Provider>
-    </div>
-  );
-};
-
-function addTwinStoriesFromCollection(collection, chapter, renderer = twinWalletStateRender) {
-  Object.keys(collection).map(storyName => {
-    storiesOf(chapter, module).add(
-      storyName,
-      renderer(collection[storyName].a, collection[storyName].b),
-    );
-  });
-}
-
 function addStoriesFromCollection(collection, chapter, renderer = walletStateRender) {
   Object.keys(collection).map(storyName => {
     storiesOf(chapter, module).add(storyName, renderer(collection[storyName]));
@@ -80,38 +49,5 @@ const NetworkStatuses = {
 };
 
 addStoriesFromCollection(NetworkStatuses, 'Network Statuses');
-
-const TwinWalletScreensIndirectFunding = {
-  'Both in WaitForApproval': {
-    a: indirectFundingWalletStateA(indirectFundingPlayerA.WAIT_FOR_APPROVAL),
-    b: indirectFundingWalletStateB(indirectFundingPlayerB.WAIT_FOR_APPROVAL),
-  },
-  'Both in WaitForPreFundSetup': {
-    a: indirectFundingWalletStateA(indirectFundingPlayerA.WAIT_FOR_PRE_FUND_SETUP_1),
-    b: indirectFundingWalletStateB(indirectFundingPlayerB.WAIT_FOR_PRE_FUND_SETUP_0),
-  },
-  'Both in WaitForDirectFunding': {
-    a: indirectFundingWalletStateA(indirectFundingPlayerA.WAIT_FOR_DIRECT_FUNDING),
-    b: indirectFundingWalletStateB(indirectFundingPlayerB.WAIT_FOR_DIRECT_FUNDING),
-  },
-  'Both in WaitForPostFundSetup': {
-    a: indirectFundingWalletStateA(indirectFundingPlayerA.WAIT_FOR_POST_FUND_SETUP_1),
-    b: indirectFundingWalletStateB(indirectFundingPlayerB.WAIT_FOR_POST_FUND_SETUP_0),
-  },
-  'Both in WaitForLedgerUpdate': {
-    a: indirectFundingWalletStateA(indirectFundingPlayerA.WAIT_FOR_LEDGER_UPDATE_1),
-    b: indirectFundingWalletStateB(indirectFundingPlayerB.WAIT_FOR_LEDGER_UPDATE_0),
-  },
-  'B in WaitForConsensus': {
-    a: indirectFundingWalletStateA(indirectFundingPlayerA.WAIT_FOR_LEDGER_UPDATE_1),
-    b: indirectFundingWalletStateB(indirectFundingPlayerB.WAIT_FOR_CONSENSUS),
-  },
-};
-
-addTwinStoriesFromCollection(
-  TwinWalletScreensIndirectFunding,
-  'Indirect Funding Process',
-  twinWalletStateRender,
-);
 
 storiesOf('Landing Page', module).add('Landing Page', walletStateRender({}));
