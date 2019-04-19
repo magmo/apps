d5f0d71fee31db0cfba299e30f67010e1fb71350
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-04-19 10:51:04 -0600

    update actions

--- packages/wallet/src/redux/protocols/funding/player-a/actions.ts
+++ packages/wallet/src/redux/protocols/funding/player-a/actions.ts
@@ -1,10 +1,16 @@
 import { BaseProcessAction } from '../../actions';
 import { PlayerIndex } from '../../../types';
 
-export type FundingAction = StrategyChosen | StrategyApproved | StrategyRejected;
+export type FundingAction =
+  | StrategyChosen
+  | StrategyApproved
+  | FundingSuccessAcknowledged
+  | StrategyRejected
+  | Cancelled;
 
 export const STRATEGY_CHOSEN = 'WALLET.FUNDING.STRATEGY_CHOSEN';
 export const STRATEGY_APPROVED = 'WALLET.FUNDING.STRATEGY_APPROVED';
+export const FUNDING_SUCCESS_ACKNOWLEDGED = 'WALLET.FUNDING.FUNDING_SUCCESS_ACKNOWLEDGED';
 export const STRATEGY_REJECTED = 'WALLET.FUNDING.STRATEGY_REJECTED';
 export const CANCELLED = 'WALLET.FUNDING.CANCELLED';
 export const CANCELLED_BY_OPPONENT = 'WALLET.FUNDING.CANCELLED_BY_OPPONENT';
@@ -15,6 +21,10 @@ export interface StrategyChosen extends BaseProcessAction {
 
 export interface StrategyApproved extends BaseProcessAction {
   type: typeof STRATEGY_APPROVED;
+}
+
+export interface FundingSuccessAcknowledged extends BaseProcessAction {
+  type: typeof FUNDING_SUCCESS_ACKNOWLEDGED;
 }
 
 export interface StrategyRejected extends BaseProcessAction {
@@ -37,6 +47,11 @@ export const strategyChosen = (processId: string): StrategyChosen => ({
 
 export const strategyApproved = (processId: string): StrategyApproved => ({
   type: STRATEGY_APPROVED,
+  processId,
+});
+
+export const fundingSuccessAcknowledged = (processId: string): FundingSuccessAcknowledged => ({
+  type: FUNDING_SUCCESS_ACKNOWLEDGED,
   processId,
 });
 
