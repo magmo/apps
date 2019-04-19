0267590fab69d413b47394a2a881c92f64492a13
Mike Kerzhner <mikerzhner@gmail.com>
2019-04-18 17:04:06 -0700

    Some of the direct funding UI

---
+++ packages/wallet/src/redux/protocols/direct-funding/__tests__/stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Modal from 'react-modal';
+import { Provider } from 'react-redux';
+import DirectFundingContainer from '../container';
+import { happyPathA } from './scenarios';
+import * as storybookUtils from '../../../../__stories__/index';
+
+const render = state => () => {
+  const container = <DirectFundingContainer directFundingState={state} />;
+  return (
+    <Provider store={storybookUtils.fakeStore({})}>
+      <Modal
+        isOpen={true}
+        className={'wallet-content-center'}
+        overlayClassName={'wallet-overlay-center'}
+        ariaHideApp={false}
+      >
+        {container}
+      </Modal>
+    </Provider>
+  );
+};
+
+const directFundingScreens = {
+  NotSafeToDeposit: happyPathA.states.notSafeToDeposit,
+  // WaitForTransactionSubmission: happyPathA.states.waitForTransactionSubmission,
+  WaitForFundingConfirmation: happyPathA.states.waitForFundingConfirmation,
+  ChannelFunded: happyPathA.states.channelFunded,
+};
+
+storybookUtils.addStoriesFromCollection(directFundingScreens, 'Direct Funding Happy Path', render);
