80d444f89bfff16d6d64807ba4fb6744144a5f16
Alex Gap <alex.gap@gmail.com>
2019-04-24 15:07:50 -0700

    Remove old state

--- packages/wallet/src/redux/protocols/funding/states.ts
+++ packages/wallet/src/redux/protocols/funding/states.ts
@@ -11,7 +11,6 @@ export { playerA, playerB };
 export function isPlayerAFundingState(state: FundingState): state is PlayerAFundingState {
   return (
     state.type === playerA.WAIT_FOR_FUNDING ||
-    state.type === playerA.WAIT_FOR_POSTFUND_SETUP ||
     state.type === playerA.WAIT_FOR_STRATEGY_CHOICE ||
     state.type === playerA.WAIT_FOR_STRATEGY_RESPONSE ||
     state.type === playerA.WAIT_FOR_SUCCESS_CONFIRMATION
