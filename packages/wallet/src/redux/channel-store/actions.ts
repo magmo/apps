4b17bd755b90671e92262cd478b1d75848237ef1
Tom Close <tom.close@cantab.net>
2019-04-26 09:20:00 +0100

    More cleanup

--- packages/wallet/src/redux/channel-store/actions.ts
+++ packages/wallet/src/redux/channel-store/actions.ts
@@ -1,11 +1,6 @@
 import { Commitment } from '../../domain';
 import * as walletActions from '../actions';
 
-export const CREATE_CHANNEL_REQUEST = ''; // send over opponent addresses, gameLibrary
-// return nonce etc.
-export const JOIN_CHANNEL_REQUEST = '';
-export const ADDRESS_REQUEST = ''; // provide me with an address
-
 export const OWN_COMMITMENT_RECEIVED = 'WALLET.CHANNEL.OWN_COMMITMENT_RECEIVED';
 export const ownCommitmentReceived = (commitment: Commitment) => ({
   type: OWN_COMMITMENT_RECEIVED as typeof OWN_COMMITMENT_RECEIVED,
@@ -26,9 +21,3 @@ export type ChannelAction =  // TODO: Some of these actions probably also belong
   | OwnCommitmentReceived
   | walletActions.CommonAction
   | walletActions.internal.InternalChannelAction;
-
-export const isReceiveFirstCommitment = (
-  action: walletActions.WalletAction,
-): action is OwnCommitmentReceived | OpponentCommitmentReceived => {
-  return 'commitment' in action && action.commitment.turnNum === 0;
-};
