3048fc54d803e2cef359bbafd290cce2c7c9e178
Tom Close <tom.close@cantab.net>
2019-04-28 09:35:08 +0100

    Finish tests for indirect funding

--- packages/wallet/src/redux/protocols/indirect-funding/player-a/__tests__/scenarios.ts
+++ packages/wallet/src/redux/protocols/indirect-funding/player-a/__tests__/scenarios.ts
@@ -1,10 +1,10 @@
 import { bigNumberify } from 'ethers/utils';
 import * as globalActions from '../../../../actions';
 import {
-  bWaitForPreFundSetup0,
-  bWaitForDirectFunding,
-  bWaitForLedgerUpdate0,
-  bWaitForPostFundSetup0,
+  aWaitForPreFundSetup1,
+  aWaitForDirectFunding,
+  aWaitForLedgerUpdate1,
+  aWaitForPostFundSetup1,
 } from '../state';
 import { channelFromCommitments } from '../../../../channel-store/channel-state/__tests__';
 import { EMPTY_SHARED_DATA, setChannels } from '../../../../state';
@@ -20,7 +20,7 @@ import {
   ledgerCommitment,
   asAddress,
   bsAddress,
-  bsPrivateKey,
+  asPrivateKey,
   ledgerId,
   channelId,
 } from '../../../../../domain/commitments/__tests__';
@@ -44,7 +44,6 @@ const app3 = appCommitment({ turnNum: 3, balances: twoThree });
 
 const ledger0 = ledgerCommitment({ turnNum: 0, balances: twoThree });
 const ledger1 = ledgerCommitment({ turnNum: 1, balances: twoThree });
-const ledger2 = ledgerCommitment({ turnNum: 2, balances: twoThree });
 const ledger3 = ledgerCommitment({ turnNum: 3, balances: twoThree });
 const ledger4 = ledgerCommitment({ turnNum: 4, balances: twoThree, proposedBalances: fiveToApp });
 const ledger5 = ledgerCommitment({ turnNum: 5, balances: fiveToApp });
@@ -56,71 +55,72 @@ const props = { channelId, ledgerId };
 // ------
 // States
 // ------
-const waitForPreFundL0 = {
-  state: bWaitForPreFundSetup0(props),
+const waitForPreFundL1 = {
+  state: aWaitForPreFundSetup1(props),
   store: setChannels(EMPTY_SHARED_DATA, [
-    channelFromCommitments(app0, app1, bsAddress, bsPrivateKey),
+    channelFromCommitments(app0, app1, asAddress, asPrivateKey),
+    channelFromCommitments(undefined, ledger0, asAddress, asPrivateKey),
   ]),
 };
 const waitForDirectFunding = {
-  state: bWaitForDirectFunding({ ...props, directFundingState: preSuccessStateB.protocolState }), //
+  state: aWaitForDirectFunding({ ...props, directFundingState: preSuccessStateB.protocolState }), //
   store: setChannels(preSuccessStateB.sharedData, [
-    channelFromCommitments(app0, app1, bsAddress, bsPrivateKey),
-    channelFromCommitments(ledger0, ledger1, bsAddress, bsPrivateKey),
+    channelFromCommitments(app0, app1, asAddress, asPrivateKey),
+    channelFromCommitments(ledger0, ledger1, asAddress, asPrivateKey),
   ]),
 };
-const waitForLedgerUpdate0 = {
-  state: bWaitForLedgerUpdate0(props),
+const waitForLedgerUpdate1 = {
+  state: aWaitForLedgerUpdate1(props),
   store: setChannels(EMPTY_SHARED_DATA, [
-    channelFromCommitments(app0, app1, bsAddress, bsPrivateKey),
-    channelFromCommitments(ledger2, ledger3, bsAddress, bsPrivateKey),
+    channelFromCommitments(app0, app1, asAddress, asPrivateKey),
+    channelFromCommitments(ledger3, ledger4, asAddress, asPrivateKey),
   ]),
 };
-const waitForPostFund0 = {
-  state: bWaitForPostFundSetup0(props),
+const waitForPostFund1 = {
+  state: aWaitForPostFundSetup1(props),
   store: setChannels(EMPTY_SHARED_DATA, [
-    channelFromCommitments(app0, app1, bsAddress, bsPrivateKey),
-    channelFromCommitments(ledger4, ledger5, bsAddress, bsPrivateKey),
+    channelFromCommitments(app0, app1, asAddress, asPrivateKey),
+    channelFromCommitments(ledger4, ledger5, asAddress, asPrivateKey),
   ]),
 };
 
 const waitForDirectFundingFailure = {
-  state: bWaitForDirectFunding({ ...props, directFundingState: preFailureState.protocolState }), //
+  state: aWaitForDirectFunding({ ...props, directFundingState: preFailureState.protocolState }), //
   store: setChannels(preFailureState.sharedData, [
-    channelFromCommitments(app0, app1, bsAddress, bsPrivateKey),
-    channelFromCommitments(ledger0, ledger1, bsAddress, bsPrivateKey),
+    channelFromCommitments(app0, app1, asAddress, asPrivateKey),
+    channelFromCommitments(ledger0, ledger1, asAddress, asPrivateKey),
   ]),
 };
 
 // -------
 // Actions
 // -------
-const preFundL0Received = globalActions.commitmentReceived(
+const preFundL1Received = globalActions.commitmentReceived(
   processId,
-  ledger0.commitment,
-  ledger0.signature,
+  ledger1.commitment,
+  ledger1.signature,
 );
-const ledgerUpdate0Received = globalActions.commitmentReceived(
+const ledgerUpdate1Received = globalActions.commitmentReceived(
   processId,
-  ledger4.commitment,
-  ledger4.signature,
+  ledger5.commitment,
+  ledger5.signature,
 );
-const postFund0Received = globalActions.commitmentReceived(
+const postFund1Received = globalActions.commitmentReceived(
   processId,
-  app2.commitment,
-  app2.signature,
+  app3.commitment,
+  app3.signature,
 );
 
 export const happyPath = {
-  initialParams: { store: waitForPreFundL0.store, channelId },
-  waitForPreFundL0: { state: waitForPreFundL0, action: preFundL0Received, reply: ledger1 },
-  waitForDirectFunding: { state: waitForDirectFunding, action: successTriggerB },
-  waitForLedgerUpdate0: {
-    state: waitForLedgerUpdate0,
-    action: ledgerUpdate0Received,
-    reply: ledger5,
+  initialParams: { store: waitForPreFundL1.store, channelId, reply: ledger0 },
+  waitForPreFundL1: { state: waitForPreFundL1, action: preFundL1Received },
+  waitForDirectFunding: { state: waitForDirectFunding, action: successTriggerB, reply: ledger4 },
+  waitForLedgerUpdate1: {
+    state: waitForLedgerUpdate1,
+    action: ledgerUpdate1Received,
+    reply: app2,
   },
-  waitForPostFund0: { state: waitForPostFund0, action: postFund0Received, reply: app3 },
+  waitForPostFund1: { state: waitForPostFund1, action: postFund1Received },
 };
 
 export const ledgerFundingFails = {
