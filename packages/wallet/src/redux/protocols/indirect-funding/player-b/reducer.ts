be5a85feac47059de3de66251902d047e78918dd
Tom Close <tom.close@cantab.net>
2019-04-30 12:16:16 +0100

    Work on the indirect-funding reducer

--- packages/wallet/src/redux/protocols/indirect-funding/player-b/reducer.ts
+++ packages/wallet/src/redux/protocols/indirect-funding/player-b/reducer.ts
@@ -4,10 +4,24 @@ import { PlayerBState } from '../state';
 import * as actions from '../../../actions';
 
 import { ProtocolStateWithSharedData } from '../../';
-import { SharedData } from '../../../state';
+import { SharedData, checkAndStore, getChannel, signAndStore, queueMessage } from '../../../state';
 import { IndirectFundingState } from '../state';
+import { unreachable } from '../../../../utils/reducer-utils';
+import {
+  BWaitForPreFundSetup0,
+  BWaitForDirectFunding,
+  BWaitForLedgerUpdate0,
+  BWaitForPostFundSetup0,
+  bWaitForDirectFunding,
+} from './state';
+import { getChannelId, nextSetupCommitment } from '../../../../domain';
+import { CONSENSUS_LIBRARY_ADDRESS } from 'src/constants';
+import { createCommitmentMessageRelay } from '../../reducer-helpers';
+import { theirAddress } from 'src/redux/channel-store';
+import { initialDirectFundingState } from '../../direct-funding/state';
 
 type ReturnVal = ProtocolStateWithSharedData<IndirectFundingState>;
+type IDFAction = actions.indirectFunding.Action;
 
 export function initialize(channelId: string, sharedData: SharedData): ReturnVal {
   // todo: check that channel exists?
@@ -17,7 +31,100 @@ export function initialize(channelId: string, sharedData: SharedData): ReturnVal
 export function playerBReducer(
   protocolState: PlayerBState,
   sharedData: SharedData,
-  action: actions.indirectFunding.Action,
+  action: IDFAction,
+): ReturnVal {
+  switch (protocolState.type) {
+    case 'BWaitForPreFundSetup0':
+      return handleWaitForPreFundSetup(protocolState, sharedData, action);
+    case 'BWaitForDirectFunding':
+      return handleWaitForDirectFunding(protocolState, sharedData, action);
+    case 'BWaitForLedgerUpdate0':
+      return handleWaitForLedgerUpdate(protocolState, sharedData, action);
+    case 'BWaitForPostFundSetup0':
+      return handleWaitForPostFundSetup(protocolState, sharedData, action);
+    default:
+      return unreachable(protocolState);
+  }
+}
+
+function handleWaitForPreFundSetup(
+  protocolState: BWaitForPreFundSetup0,
+  sharedData: SharedData,
+  action: IDFAction,
+): ReturnVal {
+  const unchangedState = { protocolState, sharedData };
+  if (action.type !== actions.COMMITMENT_RECEIVED) {
+    return unchangedState;
+  }
+
+  const checkResult = checkAndStore(sharedData, action.signedCommitment);
+  if (!checkResult.isSuccess) {
+    return unchangedState;
+  }
+  sharedData = checkResult.store;
+
+  // at this point we don't know for sure that it was a pre-fund setup.
+  // they could've sent us any valid update
+
+  const theirCommitment = action.signedCommitment.commitment;
+  const ledgerId = getChannelId(theirCommitment);
+  let channel = getChannel(sharedData, ledgerId);
+  if (!channel || channel.turnNum !== 0 || channel.libraryAddress !== CONSENSUS_LIBRARY_ADDRESS) {
+    // todo: this could be more robust somehow.
+    // Maybe we should generate what we were expecting and compare.
+    return unchangedState;
+  }
+
+  // at this point we're happy that we have the pre-fund setup
+  // we need to craft our reply
+
+  const ourCommitment = nextSetupCommitment(theirCommitment);
+  if (ourCommitment === 'NotASetupCommitment') {
+    return unchangedState;
+  }
+  const signResult = signAndStore(sharedData, ourCommitment);
+  if (!signResult.isSuccess) {
+    return unchangedState;
+  }
+  sharedData = signResult.store;
+
+  // just need to put our message in the outbox
+  const messageRelay = createCommitmentMessageRelay(
+    theirAddress(channel),
+    'processId',
+    signResult.signedCommitment.commitment,
+    signResult.signedCommitment.signature,
+  );
+  sharedData = queueMessage(sharedData, messageRelay);
+  channel = getChannel(sharedData, ledgerId); // refresh channel
+
+  // update the state
+  const directFundingState = initialDirectFundingState();
+  protocolState = bWaitForDirectFunding({ ...protocolState, ledgerId, directFundingState });
+
+  return { protocolState, sharedData };
+}
+
+function handleWaitForDirectFunding(
+  protocolState: BWaitForDirectFunding,
+  sharedData: SharedData,
+  action: IDFAction,
+): ReturnVal {
+  return { protocolState, sharedData };
+}
+
+function handleWaitForLedgerUpdate(
+  protocolState: BWaitForLedgerUpdate0,
+  sharedData: SharedData,
+  action: IDFAction,
+): ReturnVal {
+  return { protocolState, sharedData };
+}
+
+function handleWaitForPostFundSetup(
+  protocolState: BWaitForPostFundSetup0,
+  sharedData: SharedData,
+  action: IDFAction,
 ): ReturnVal {
   return { protocolState, sharedData };
 }
