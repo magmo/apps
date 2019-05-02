d6da03435c14f61812a57628fca8bddd708e7eca
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-01 17:04:34 -0700

    Improve magmo-wallet-client index

--- packages/magmo-wallet-client/src/index.ts
+++ packages/magmo-wallet-client/src/index.ts
@@ -3,8 +3,13 @@
  * The wallet client.
  */
 
-export * from './wallet-events';
-
-export * from './wallet-functions';
-export { WalletEventListener } from './wallet-event-listener';
 export * from './wallet-types';
+export * from './wallet-events';
+export * from './wallet-functions';
+export * from './wallet-instructions';
+
+import * as walletEvents from './wallet-events';
+import * as walletFunctions from './wallet-functions';
+import * as walletInstructions from './wallet-instructions';
+import { WalletEventListener } from './wallet-event-listener';
+export { walletEvents, walletFunctions, walletInstructions, WalletEventListener };
