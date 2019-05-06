83cb74e6f1fccb0a64de934c700480e9a92c6391
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-06 09:41:53 -0600

    Rename imports

--- packages/wallet/src/redux/protocols/funding/actions.ts
+++ packages/wallet/src/redux/protocols/funding/actions.ts
@@ -1,38 +1,31 @@
-import * as playerAFundingActions from './player-a/actions';
-import * as playerBFundingActions from './player-b/actions';
+import * as playerA from './player-a/actions';
+import * as playerB from './player-b/actions';
 import {
   isIndirectFundingAction,
   Action as IndirectFundingAction,
 } from '../indirect-funding/actions';
 
-export type FundingAction =
-  | playerAFundingActions.FundingAction
-  | playerBFundingActions.FundingAction
-  | IndirectFundingAction;
+export type FundingAction = playerA.FundingAction | playerB.FundingAction | IndirectFundingAction;
 
-export function isPlayerAFundingAction(
-  action: FundingAction,
-): action is playerAFundingActions.FundingAction {
+export function isPlayerAFundingAction(action: FundingAction): action is playerA.FundingAction {
   return (
-    action.type === playerAFundingActions.CANCELLED ||
+    action.type === playerA.CANCELLED ||
     // TODO: Add this to the check when the action exists action.type === playerAFundingActions.CANCELLED_BY_OPPONENT ||
-    action.type === playerAFundingActions.FUNDING_SUCCESS_ACKNOWLEDGED ||
-    action.type === playerAFundingActions.STRATEGY_APPROVED ||
-    action.type === playerAFundingActions.STRATEGY_CHOSEN ||
-    action.type === playerAFundingActions.STRATEGY_REJECTED ||
+    action.type === playerA.FUNDING_SUCCESS_ACKNOWLEDGED ||
+    action.type === playerA.STRATEGY_APPROVED ||
+    action.type === playerA.STRATEGY_CHOSEN ||
+    action.type === playerA.STRATEGY_REJECTED ||
     isIndirectFundingAction(action)
   );
 }
-export function isPlayerBFundingAction(
-  action: FundingAction,
-): action is playerBFundingActions.FundingAction {
+export function isPlayerBFundingAction(action: FundingAction): action is playerB.FundingAction {
   return (
-    action.type === playerBFundingActions.CANCELLED ||
+    action.type === playerB.CANCELLED ||
     // TODO: Add this to the check when the action exists action.type === playerAFundingActions.CANCELLED_BY_OPPONENT ||
-    action.type === playerBFundingActions.FUNDING_SUCCESS_ACKNOWLEDGED ||
-    action.type === playerBFundingActions.STRATEGY_APPROVED ||
-    action.type === playerBFundingActions.STRATEGY_PROPOSED ||
-    action.type === playerBFundingActions.STRATEGY_REJECTED ||
+    action.type === playerB.FUNDING_SUCCESS_ACKNOWLEDGED ||
+    action.type === playerB.STRATEGY_APPROVED ||
+    action.type === playerB.STRATEGY_PROPOSED ||
+    action.type === playerB.STRATEGY_REJECTED ||
     isIndirectFundingAction(action)
   );
 }
