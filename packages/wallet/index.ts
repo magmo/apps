c76e9dc157c1ac0469b13c4534d0b480713ec82f
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-10 16:51:33 -0600

    Export test scenarios as part of communication module

--- packages/wallet/index.ts
+++ packages/wallet/index.ts
@@ -1,5 +1,11 @@
-import * as communication from './src/communication';
+import * as communicationModule from './src/communication';
 import { RelayableAction } from './src/communication';
 import { unreachable } from './src/utils/reducer-utils';
+
+// We don't want to export test scenarios from the communication module, but in
+// other packages that want to use the communication module, we want them
+// to have access to the same test scenarios
+import communicationScenarios from './src/communication/__tests__/test-scenarios';
+const communication = { ...communicationModule, testScenarios: communicationScenarios };
 
 export { communication, RelayableAction, unreachable };
