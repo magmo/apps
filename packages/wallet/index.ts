1b26f542ee59e555e58cf36c3a2254a5dfe0b373
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-10 14:49:02 -0600

    export unreachable from wallet module

--- packages/wallet/index.ts
+++ packages/wallet/index.ts
@@ -1,4 +1,5 @@
 import * as communication from './src/communication';
 import { RelayableAction } from './src/communication';
+import { unreachable } from './src/utils/reducer-utils';
 
-export { communication, RelayableAction };
+export { communication, RelayableAction, unreachable };
