a08b92bccb94d9873a03cb2723f51990cb830799
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-13 14:28:46 -0600

    Export some helper functions from wallet index

--- packages/wallet/index.ts
+++ packages/wallet/index.ts
@@ -1,11 +1,14 @@
 import * as communicationModule from './src/communication';
 import { RelayableAction } from './src/communication';
 import { unreachable } from './src/utils/reducer-utils';
+import { getProcessId } from './src/redux/reducer';
 
 // We don't want to export test scenarios from the communication module, but in
 // other packages that want to use the communication module, we want them
 // to have access to the same test scenarios
 import communicationScenarios from './src/communication/__tests__/test-scenarios';
+import { getChannelId } from './src/domain';
+import { WalletProtocol } from './src/redux/types';
 const communication = { ...communicationModule, testScenarios: communicationScenarios };
 
-export { communication, RelayableAction, unreachable };
+export { communication, RelayableAction, unreachable, getChannelId, getProcessId, WalletProtocol };
