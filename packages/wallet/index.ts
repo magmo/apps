207012a7fd6cfc16e064192c3957745b9afb7dbe
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-21 14:09:18 -0600

    Don't export communication code from index.ts
    The communication module exports its api from its own index.ts

--- packages/wallet/index.ts
+++ packages/wallet/index.ts
@@ -1,20 +1,4 @@
-import * as communicationModule from './src/communication';
-import {
-  RelayableAction,
-  CommitmentReceived,
-  StrategyProposed,
-  ConcludeInstigated,
-} from './src/communication';
 import { unreachable } from './src/utils/reducer-utils';
-import { getProcessId } from './src/redux/reducer';
-
-// We don't want to export test scenarios from the communication module, but in
-// other packages that want to use the communication module, we want them
-// to have access to the same test scenarios
-import communicationScenarios from './src/communication/__tests__/test-scenarios';
 import { getChannelId } from './src/domain';
-import { WalletProtocol } from './src/redux/types';
-const communication = { ...communicationModule, testScenarios: communicationScenarios };
 
-export { communication, unreachable, getChannelId, getProcessId, WalletProtocol };
-export { RelayableAction, CommitmentReceived, StrategyProposed, ConcludeInstigated };
+export { unreachable, getChannelId };
