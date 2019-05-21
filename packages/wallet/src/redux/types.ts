3a96eee948180d104bc7bd5d79eff3af9c5eaa11
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-05-21 14:06:13 -0600

    WalletProtocol only includes protocols running in a process
    WalletProtocol sort of needs to be used by anyone running a wallet,
    since it's used as part of the process id synchronization, and
    communication.
    
    Thus, it doesn't make sense for WalletProtocol to include a protocol
    that's specific to eg. the react client wallet.
    
    Since these protocols are never referenced, they're removed.

--- packages/wallet/src/redux/types.ts
+++ packages/wallet/src/redux/types.ts
@@ -3,12 +3,6 @@ export const enum WalletProtocol {
   Funding = 'Funding',
   Dispute = 'Dispute',
   Concluding = 'Concluding',
-
-  Withdrawing = 'Withdrawing',
-  DirectFunding = 'DirectFunding',
-  IndirectFunding = 'IndirectFunding',
-  TransactionSubmission = 'TransactionSubmission',
-  IndirectDefunding = 'IndirectDefunding',
 }
 
 export const enum PlayerIndex {
