9ae778a6d45513658513d175661f747e85ea6a0c
George Knee <georgeknee@googlemail.com>
2019-05-26 15:26:48 +0100

    Update actions

--- packages/wallet/src/redux/protocols/indirect-defunding/actions.ts
+++ packages/wallet/src/redux/protocols/indirect-defunding/actions.ts
@@ -1,18 +1,59 @@
-import { CommitmentReceived, WalletAction } from '../../actions';
+import { CommitmentReceived, WalletAction, ChallengeCreatedEvent } from '../../actions';
+import { CommitmentType } from 'fmg-core';
+import { Commitment } from 'src/domain';
+import { ActionConstructor } from 'src/redux/utils';
+
 // -------
 // Actions
 // -------
+
+export interface UpdateConfirmed {
+  type: 'WALLET.INDIRECT_DEFUNDING.UPDATE_CONFIRMED';
+  commitmentType: CommitmentType.App | CommitmentType.Conclude;
+}
+
+export interface ChallengeChosen {
+  type: 'WALLET.INDIRECT_DEFUNDING.CHALLENGE_CHOSEN';
+  challengeCommitment: Commitment;
+}
+
+export interface Acknowledged {
+  type: 'WALLET.INDIRECT_DEFUNDING.ACKNOWLEDGED';
+}
 
 // --------
 // Constructors
 // --------
 
+export const updateConfirmed: ActionConstructor<UpdateConfirmed> = p => {
+  return { ...p, type: 'WALLET.INDIRECT_DEFUNDING.UPDATE_CONFIRMED' };
+};
+
+export const challengeChosen: ActionConstructor<ChallengeChosen> = p => {
+  return { ...p, type: 'WALLET.INDIRECT_DEFUNDING.CHALLENGE_CHOSEN' };
+};
+
+export const acknowledged: ActionConstructor<Acknowledged> = p => {
+  return { ...p, type: 'WALLET.INDIRECT_DEFUNDING.ACKNOWLEDGED' };
+};
+
 // --------
 // Unions and Guards
 // --------
 
-export type IndirectDefundingAction = CommitmentReceived;
+export type IndirectDefundingAction =
+  | CommitmentReceived
+  | UpdateConfirmed
+  | ChallengeChosen
+  | ChallengeCreatedEvent
+  | Acknowledged;
 
 export function isIndirectDefundingAction(action: WalletAction): action is IndirectDefundingAction {
-  return action.type === 'WALLET.COMMON.COMMITMENT_RECEIVED';
+  return (
+    action.type === 'WALLET.COMMON.COMMITMENT_RECEIVED' ||
+    action.type === 'WALLET.ADJUDICATOR.CHALLENGE_CREATED_EVENT' ||
+    action.type === 'WALLET.INDIRECT_DEFUNDING.UPDATE_CONFIRMED' ||
+    action.type === 'WALLET.INDIRECT_DEFUNDING.CHALLENGE_CHOSEN' ||
+    action.type === 'WALLET.INDIRECT_DEFUNDING.ACKNOWLEDGED'
+  );
 }
