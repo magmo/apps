7dfefc76f3ddbe7443dac3181e549efbd0edd3e6
George Knee <georgeknee@googlemail.com>
2019-06-14 11:18:41 +0100

    Message sender saga dispatches concluded action

--- packages/wallet/src/redux/sagas/message-sender.ts
+++ packages/wallet/src/redux/sagas/message-sender.ts
@@ -1,9 +1,17 @@
 import { put } from 'redux-saga/effects';
 import { messageSent } from '../actions';
+import { concluded } from '../protocols/application/actions';
 
 export function* messageSender(message) {
   window.parent.postMessage(message, '*');
-
+  console.log(message);
+  if (message.messagePayload) {
+    const action = message.messagePayload;
+    if (action.type === 'WALLET.NEW_PROCESS.CONCLUDE_INSTIGATED') {
+      const processId = `${action.protocol}-${action.channelId}`;
+      yield put(concluded({ processId }));
+    }
+  }
   yield put(message);
   yield put(messageSent({}));
 }
