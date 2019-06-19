bb7b1155db6d610d937a2ac7940bf0397fcdeb4b
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-06-19 15:11:43 -0600

    Add some tests

--- packages/wallet/src/redux/protocols/advance-channel/__tests__/reducer.test.ts
+++ packages/wallet/src/redux/protocols/advance-channel/__tests__/reducer.test.ts
@@ -1,24 +1,38 @@
 import * as states from '../states';
-import { initialize } from '../reducer';
+import { initialize, reducer } from '../reducer';
 import * as scenarios from './scenarios';
 import { CommitmentType } from '../../../../domain';
+import { expectThisCommitmentSent } from '../../../__tests__/helpers';
 
 const itTransitionsTo = (
-  result: { protocolState: states.AdvanceChannelState },
+  result: states.AdvanceChannelState,
   type: states.AdvanceChannelStateType,
 ) => {
   it(`transitions to ${type}`, () => {
-    expect(result.protocolState.type).toEqual(type);
+    expect(result.type).toEqual(type);
   });
 };
 
 describe('sending preFundSetup as A', () => {
   const scenario = scenarios.newChannelAsA;
+  const { channelId } = scenario;
 
   describe('when initializing', () => {
-    const { channelId } = scenario;
-    const { sharedData } = scenario.commitmentSent;
+    const { sharedData, commitment } = scenario.initialize;
     const result = initialize(channelId, sharedData, CommitmentType.PreFundSetup);
-    itTransitionsTo(result, 'AdvanceChannel.CommitmentSent');
+    itTransitionsTo(result.protocolState, 'AdvanceChannel.CommitmentSent');
+    expectThisCommitmentSent(result, commitment);
+  });
+
+  describe('when receiving prefund commitments from b', () => {
+    const { sharedData } = scenario.receiveFromB;
+    const result = initialize(channelId, sharedData, CommitmentType.PreFundSetup);
+    itTransitionsTo(result.protocolState, 'AdvanceChannel.CommitmentSent');
+  });
+
+  describe('when receiving prefund commitments from the hub', () => {
+    const { state, sharedData, action } = scenario.receiveFromHub;
+    const result = reducer(state, sharedData, action).protocolState;
+    itTransitionsTo(result, 'AdvanceChannel.Success');
   });
 });
