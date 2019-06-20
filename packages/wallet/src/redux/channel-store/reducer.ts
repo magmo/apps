e19e98d097d845b13180ce5bb5e73ea002139c5a
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-06-20 14:39:25 -0600

    Revert "checkAndStore with a stale commitment is successful, but does not update the store"
    This reverts commit 2802b71eaacf53836be5f85bbe4331e86f14f608.

--- packages/wallet/src/redux/channel-store/reducer.ts
+++ packages/wallet/src/redux/channel-store/reducer.ts
@@ -148,10 +148,6 @@ export function checkAndStore(
     return { isSuccess: false };
   }
 
-  if (commitment.turnNum <= channel.turnNum) {
-    return { isSuccess: true, store };
-  }
-
   if (!isSafeTransition(store, channel, commitment)) {
     console.log('Failed to verify a safe transition');
     return { isSuccess: false };
