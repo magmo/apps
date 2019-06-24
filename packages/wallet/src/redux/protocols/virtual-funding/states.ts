b64227bd6113bfb6ee134cd74ed72a27bdcf77ef
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-06-24 16:00:01 -0600

    Name the state properties with descriptors
    These descriptors can be used to route actions in the reducer

--- packages/wallet/src/redux/protocols/virtual-funding/states.ts
+++ packages/wallet/src/redux/protocols/virtual-funding/states.ts
@@ -10,19 +10,28 @@ interface Base {
   processId: string;
   targetChannelId: string;
 }
+export const JOINT_CHANNEL_DESCRIPTOR = 'jointChannel';
+export const GUARANTOR_CHANNEL_DESCRIPTOR = 'guarantorChannel';
+export const INDIRECT_GUARANTOR_FUNDING_DESCRIPTOR = 'indirectGuarantorFunding';
+export const INDIRECT_APPLICATION_FUNDING_DESCRIPTOR = 'indirectApplicationFunding';
+export type SUBSTATE_DESCRIPTORS =
+  | typeof JOINT_CHANNEL_DESCRIPTOR
+  | typeof GUARANTOR_CHANNEL_DESCRIPTOR
+  | typeof INDIRECT_GUARANTOR_FUNDING_DESCRIPTOR
+  | typeof INDIRECT_APPLICATION_FUNDING_DESCRIPTOR;
 
 export interface WaitForChannelPreparation extends Base {
   type: 'VirtualFunding.WaitForChannelPreparation';
-  jointChannelState: AdvanceChannelState;
-  guarantorChannelState: AdvanceChannelState;
+  [JOINT_CHANNEL_DESCRIPTOR]: AdvanceChannelState;
+  [GUARANTOR_CHANNEL_DESCRIPTOR]: AdvanceChannelState;
 }
 export interface WaitForGuarantorFunding extends Base {
   type: 'VirtualFunding.WaitForGuarantorFunding';
-  indirectFundingState: 'UpdateConsensusState<G>';
+  [INDIRECT_GUARANTOR_FUNDING_DESCRIPTOR]: 'UpdateConsensusState<G>';
 }
 export interface WaitForApplicationFunding extends Base {
   type: 'VirtualFunding.WaitForApplicationFunding';
-  indirectFundingState: 'UpdateConsensusState<J>';
+  [INDIRECT_APPLICATION_FUNDING_DESCRIPTOR]: 'UpdateConsensusState<J>';
 }
 export interface Success {
   type: 'VirtualFunding.Success';
