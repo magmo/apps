2cd0a14d03c4019e76701e2063db125868d73bc1
George Knee <georgeknee@googlemail.com>
2019-06-26 10:55:43 +0100

    Remove all from saga (do not dispatch concurrently

--- packages/wallet/src/redux/sagas/multiple-action-dispatcher.ts
+++ packages/wallet/src/redux/sagas/multiple-action-dispatcher.ts
@@ -1,9 +1,9 @@
 import { MultipleWalletActions } from '../actions';
-import { take, all, put } from 'redux-saga/effects';
+import { take, put } from 'redux-saga/effects';
 
 export function* multipleActionDispatcher() {
   while (true) {
     const multipleWalletActions: MultipleWalletActions = yield take('WALLET.MULTIPLE_ACTIONS');
-    yield all(multipleWalletActions.actions.map(action => put(action)));
+    yield multipleWalletActions.actions.map(action => put(action));
   }
 }
