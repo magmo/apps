f0645b2171c5918de2e738ad13270bc35b6ef2b3
George Knee <georgeknee@googlemail.com>
2019-07-01 16:28:28 +0100

    Add story for first scenario

---
+++ packages/wallet/src/redux/protocols/existing-ledger-funding/__tests__/stories.tsx
@@ -0,0 +1,7 @@
+import { addStoriesFromScenario as addStories } from '../../../../__stories__';
+import * as scenarios from './scenarios';
+
+addStories(
+  scenarios.playerAFullyFundedHappyPath,
+  'Existing Ledger Funding / Player A Fully Funded Happy Path',
+);
