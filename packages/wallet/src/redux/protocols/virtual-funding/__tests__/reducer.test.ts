e1ee2b9bcb9f3b094f1c41d5b6111a9c26ffee41
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-07-03 12:49:09 -0600

    Update test

--- packages/wallet/src/redux/protocols/virtual-funding/__tests__/reducer.test.ts
+++ packages/wallet/src/redux/protocols/virtual-funding/__tests__/reducer.test.ts
@@ -39,22 +39,21 @@ describe('happyPath', () => {
     itSendsTheseCommitments(result, [{ commitment: { turnNum: 0, allocation: oneTwoThree } }]);
   });
 
+  describe.only(scenarioStepDescription(scenario.openJ), () => {
+    const { state, sharedData, action } = scenario.openJ;
+    const { protocolState } = reducer(state, sharedData, action);
+
+    itTransitionsTo(protocolState, 'VirtualFunding.WaitForGuarantorChannel');
+    itTransitionsSubstateTo(protocolState, 'guarantorChannel', preSuccess.state.type);
+  });
+
   describe(scenarioStepDescription(scenario.openG), () => {
     const { state, sharedData, action } = scenario.openG;
     const { protocolState, sharedData: result } = reducer(state, sharedData, action);
 
-    itTransitionsTo(protocolState, 'VirtualFunding.WaitForGuarantorChannel');
+    itTransitionsTo(protocolState, 'VirtualFunding.WaitForGuarantorFunding');
     itTransitionsSubstateTo(protocolState, 'guarantorChannel', success.state.type);
     itTransitionsSubstateTo(protocolState, 'jointChannel', preSuccess.state.type);
     itSendsNoMessage(result);
-  });
-
-  describe(scenarioStepDescription(scenario.openJ), () => {
-    const { state, sharedData, action } = scenario.openJ;
-    const { protocolState } = reducer(state, sharedData, action);
-
-    itTransitionsTo(protocolState, 'VirtualFunding.WaitForGuarantorFunding');
-    itTransitionsSubstateTo(protocolState, 'jointChannel', success.state.type);
-    itTransitionsSubstateTo(protocolState, 'guarantorChannel', preSuccess.state.type);
   });
 });
