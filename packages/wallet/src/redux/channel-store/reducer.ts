6e7566be07f93df5878720643bd3275e3143ee42
Alex Gap <alex.gap@gmail.com>
2019-07-08 10:17:06 -0700

    Remove error throw

--- packages/wallet/src/redux/channel-store/reducer.ts
+++ packages/wallet/src/redux/channel-store/reducer.ts
@@ -142,7 +142,6 @@ export function checkAndStore(
 
   if (!isSafeTransition(store, channel, commitment)) {
     console.log('Failed to verify a safe transition');
-    throw new Error('Failed to verify a safe transition');
     return { isSuccess: false };
   }
 
