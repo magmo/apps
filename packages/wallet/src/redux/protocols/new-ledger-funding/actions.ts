d1abb20caabfec0eafaf0a76710a0d88c565e350
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-07-17 10:14:50 -0700

    Remove consensusUpdate actions from newLedgerFunding action definition

--- packages/wallet/src/redux/protocols/new-ledger-funding/actions.ts
+++ packages/wallet/src/redux/protocols/new-ledger-funding/actions.ts
@@ -1,7 +1,6 @@
 import { WalletAction } from '../../actions';
 import { isCommonAction, EmbeddedProtocol, routerFactory } from '../../../communication';
 import { isDirectFundingAction, DirectFundingAction } from '../direct-funding/actions';
-import { ConsensusUpdateAction, isConsensusUpdateAction } from '../consensus-update';
 import { AdvanceChannelAction, isAdvanceChannelAction } from '../advance-channel';
 
 // -------
@@ -16,15 +15,11 @@ import { AdvanceChannelAction, isAdvanceChannelAction } from '../advance-channel
 // Unions and Guards
 // --------
 
-export type NewLedgerFundingAction =
-  | ConsensusUpdateAction
-  | AdvanceChannelAction
-  | DirectFundingAction;
+export type NewLedgerFundingAction = AdvanceChannelAction | DirectFundingAction;
 export function isNewLedgerFundingAction(action: WalletAction): action is NewLedgerFundingAction {
   return (
     isCommonAction(action, EmbeddedProtocol.NewLedgerFunding) ||
     isDirectFundingAction(action) ||
-    isConsensusUpdateAction(action) ||
     isAdvanceChannelAction(action)
   );
 }
