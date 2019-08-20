24155f0e0ab8a2572643a70c1dd833736911b032
Alex Gap <alex.gap@gmail.com>
2019-08-20 12:33:51 -0700

    Clean up table

--- packages/wallet/src/redux/protocols/channel-management/components/display-channels.tsx
+++ packages/wallet/src/redux/protocols/channel-management/components/display-channels.tsx
@@ -13,38 +13,65 @@ export interface Props {
 export default class DisplayChannels extends React.Component<Props> {
   render() {
     const { ledgerChannels, applicationChannels, closeAction } = this.props;
-    const columns = [
+    const hubColumns = [
       {
         name: 'Channel Id',
         selector: 'channelId',
       },
-      { name: 'OpponentAddress', selector: 'opponentAddress' },
       {
-        name: 'Your Amount',
+        name: 'Your Deposit',
         selector: 'ourAmount',
         cell: row => <div>{formatEther(bigNumberify(row.ourAmount || '0x0'))}</div>,
+        width: '50px',
       },
-      {
-        name: 'Opponent Amount',
-        selector: 'opponentAmount',
-        cell: row => <div>{formatEther(bigNumberify(row.opponentAmount || '0x0'))}</div>,
-      },
+
       {
         name: 'Channel Total',
         selector: 'channelAmount',
         cell: row => <div>{formatEther(bigNumberify(row.channelAmount || '0x0'))}</div>,
+        width: '50px',
       },
+
       {
-        name: 'Funding Channel',
+        name: 'Status',
         selector: 'fundingChannel',
-      },
-      {
-        name: 'Funded By',
-        selector: 'fundedBy',
+        cell: row => <div>{row.fundingChannel ? `Funding ${row.channelId}` : 'Open'}</div>,
       },
     ];
-    const ledgerTable = <DataTable columns={columns} data={ledgerChannels} />;
-    const appTable = <DataTable columns={columns} data={applicationChannels} />;
+    const appColumns = [
+      {
+        name: 'Channel Id',
+        selector: 'channelId',
+      },
+      {
+        name: "Opponent's address",
+        selector: 'opponentAddress',
+      },
+      {
+        name: 'Your Funds',
+        selector: 'ourAmount',
+        cell: row => <div>{formatEther(bigNumberify(row.ourAmount || '0x0'))}</div>,
+        width: '50px',
+      },
+
+      {
+        name: "Opponent's Funds",
+        selector: 'opponentAmount',
+        cell: row => <div>{formatEther(bigNumberify(row.channelAmount || '0x0'))}</div>,
+        width: '50px',
+      },
+      {
+        name: 'Status',
+        selector: 'fundedBy',
+        cell: row => <div>{row.fundedBy ? `Funded by ${row.fundedBy}` : 'Directly Funded'}</div>,
+      },
+    ];
+    const ledgerTable = (
+      <DataTable title="Hub Channels" columns={hubColumns} data={ledgerChannels} />
+    );
+    const appTable = (
+      <DataTable title="Application Channels" columns={appColumns} data={applicationChannels} />
+    );
 
     return (
       <Fragment>
