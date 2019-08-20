b19c6286d561fcc2a92b2edc5654d11f7d899394
Alex Gap <alex.gap@gmail.com>
2019-08-20 15:55:01 -0700

    Return empty appChannelId for CloseLedgerChannel

--- packages/server/src/wallet/models/WalletProcess.ts
+++ packages/server/src/wallet/models/WalletProcess.ts
@@ -1,3 +1,4 @@
+import { ProcessProtocol } from 'magmo-wallet/lib/src/communication';
 import { Model, snakeCaseMappers } from 'objection';
 
 export default class WalletProcess extends Model {
@@ -13,6 +14,10 @@ export default class WalletProcess extends Model {
   }
 
   get appChannelId() {
-    return this.processId.split('-')[1];
+    const process = this.processId.split('-')[0];
+    if (process !== ProcessProtocol.CloseLedgerChannel) {
+      return this.processId.split('-')[1];
+    }
+    return '';
   }
 }
