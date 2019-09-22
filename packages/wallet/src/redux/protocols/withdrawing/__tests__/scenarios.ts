9c92810e9d8a354d9a62e6c48fb8eca745e5a626
Alex Gap <alex.gap@gmail.com>
2019-09-22 16:57:11 -0700

    Fix state signatures

--- packages/wallet/src/redux/protocols/withdrawing/__tests__/scenarios.ts
+++ packages/wallet/src/redux/protocols/withdrawing/__tests__/scenarios.ts
@@ -8,25 +8,24 @@ import { Wallet } from 'ethers';
 import { SharedData } from '../../../state';
 import * as web3Utils from 'web3-utils';
 import * as testScenarios from '../../../../domain/commitments/__tests__';
-import { splitSignature } from 'ethers/utils';
+
 // ---------
 // Test data
 // ---------
 
 const { channelId } = testScenarios;
-const signature = splitSignature(`0x5e9b7a7bd77ac21372939d386342ae58081a33bf53479152c87c1e787c27d06b
-       118d3eccff0ace49891e192049e16b5210047068384772ba1fdb33bbcba580391c`);
-const gameState1 = testScenarios.appState({ turnNum: 19 }).state;
-const gameState2 = testScenarios.appState({ turnNum: 20 }).state;
-const concludeState1 = testScenarios.appState({ turnNum: 51, isFinal: true }).state;
-const concludeState2 = testScenarios.appState({ turnNum: 52, isFinal: true }).state;
+
+const gameState1 = testScenarios.appState({ turnNum: 19 });
+const gameState2 = testScenarios.appState({ turnNum: 20 });
+const concludeState1 = testScenarios.appState({ turnNum: 51, isFinal: true });
+const concludeState2 = testScenarios.appState({ turnNum: 52, isFinal: true });
 
 const channelStatus: ChannelState = {
-  channel: concludeState2.channel,
+  channel: concludeState2.state.channel,
   type: 'Channel.WaitForState',
-  turnNumRecord: concludeState2.turnNum,
+  turnNumRecord: concludeState2.state.turnNum,
 
-  signedStates: [{ state: concludeState1, signature }, { state: concludeState2, signature }],
+  signedStates: [concludeState1, concludeState2],
 };
 
 const channelStore: ChannelStore = {
@@ -35,8 +34,8 @@ const channelStore: ChannelStore = {
 
 const notClosedChannelStatus: ChannelState = {
   ...channelStatus,
-  signedStates: [{ state: gameState1, signature }, { state: gameState2, signature }],
-  turnNumRecord: gameState2.turnNum,
+  signedStates: [gameState1, gameState2],
+  turnNumRecord: gameState2.state.turnNum,
 };
 
 const notClosedChannelState = {
